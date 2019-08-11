b806642d6303d16e9552e398b29adb4624000f32
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2019-08-11 15:49:18 +0500

    chore: Install missing dependencies.

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,5 @@
 'use strict';
 
-module.exports = api => {
-  api.cache(true);
-
-  return {
-    presets: ['@babel/react', ['@babel/env', { modules: false }], '@babel/typescript'],
-
-    plugins: [
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-object-rest-spread',
-    ],
-  };
+module.exports = {
+  presets: ['@babel/typescript', '@babel/react'],
 };
