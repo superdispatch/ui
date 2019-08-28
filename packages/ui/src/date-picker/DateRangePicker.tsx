43a99d65761276f442dd1ce50791a1927e6f4c21
Sergey Vikhristyuk <sergey@mysuperdispatch.com>
2019-08-28 11:39:50 +0500

    refactor(ui): Use `DateRangePicker` value prop instead of local state

--- packages/ui/src/date-picker/DateRangePicker.tsx
+++ packages/ui/src/date-picker/DateRangePicker.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDateRangePickerStyles } from './DateRangePicker.styles';
 import {
   DatePickerBase,
@@ -21,27 +21,21 @@ export interface DateRangePickerProps extends DatePickerBaseProps {
 export function DateRangePicker({ value, onChange, ...props }: DateRangePickerProps) {
   const { firstDayOfRange, lastDayOfRange, ...classNames } = useDateRangePickerStyles();
   const [pickingDateType, setPickingDateType] = useState<'start' | 'end'>('start');
-  const [startDate, setStartDate] = useState(value[0]);
-  const [endDate, setEndDate] = useState(value[1]);
+  const [startDate, endDate] = value;
   const selectedDays = startDate && endDate && { from: startDate, to: endDate };
-
   const modifiers = { [firstDayOfRange]: startDate, [lastDayOfRange]: endDate };
 
   const handleDayClick = (date: Date) => {
     if (pickingDateType === 'start') {
-      setStartDate(date);
+      onChange([date, endDate]);
       setPickingDateType('end');
     }
 
     if (pickingDateType === 'end') {
-      setEndDate(date);
+      onChange([startDate, date]);
       setPickingDateType('start');
     }
   };
-
-  useEffect(() => {
-    onChange([startDate, endDate]);
-  }, [endDate, onChange, startDate]);
 
   return (
     <DatePickerBase
