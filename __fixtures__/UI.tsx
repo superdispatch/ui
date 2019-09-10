2d28e219929691722eb9101b0032b2a45af08688
Umidbek Karimov <uma.karimov@gmail.com>
2019-09-10 22:53:32 +0500

    feat(ui): Theme `Link` component. (#11)

--- __fixtures__/UI.tsx
+++ __fixtures__/UI.tsx
@@ -1,13 +1,15 @@
 import { ButtonDemo } from '../packages/ui/src/button/Button.demo';
 import { DatePickerDemo } from '../packages/ui/src/date-picker/DatePicker.demo';
+import { LinkDemo } from '../packages/ui/src/link/Link.demo';
 import { MenuDemo } from '../packages/ui/src/menu/Menu.demo';
-import { TabsDemo } from '../packages/ui/src/tabs/Tabs';
-import { TextDemo } from '../packages/ui/src/text/Text.demo';
+import { TabsDemo } from '../packages/ui/src/tabs/Tabs.demo';
+import { TypographyDemo } from '../packages/ui/src/typography/Typography.demo';
 
 export default {
   Buttons: ButtonDemo,
-  Typography: TextDemo,
+  'Date Picker': DatePickerDemo,
+  Link: LinkDemo,
   Menu: MenuDemo,
   Tabs: TabsDemo,
-  'Date Picker': DatePickerDemo,
+  Typography: TypographyDemo,
 };
