1f949aa81a9f1affbdbc65302ad5dce4c8da7286
Umidbek Karimov <uma.karimov@gmail.com>
2019-10-30 22:31:17 +0500

    feat(ui): Add borders to first and last month days when they are in selection range of `DateRangePicker`.

--- packages/ui/src/date-picker/DateRangePickerStyles.ts
+++ packages/ui/src/date-picker/DateRangePickerStyles.ts
@@ -4,27 +4,26 @@ import { Color } from '../theme/Color';
 
 function makeRangeStyles(type: 'first' | 'last'): CSSProperties {
   return {
-    '&:not(.SuperDispatchCalendar-outside)': {
-      '&$selected': {
-        color: Color.White,
-      },
-
-      '&:after': {
-        content: '""',
-        position: 'absolute',
-        zIndex: -1,
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        borderRadius: 4,
-        background: `${Color.Blue300}`,
-      },
+    '&$selected': {
+      color: Color.White,
 
       '&:before': {
-        left: type === 'first' ? '50%' : undefined,
-        right: type === 'last' ? '50%' : undefined,
+        left: type === 'first' ? 4 : undefined,
+        right: type === 'last' ? 4 : undefined,
       },
+    },
+
+    '&:after': {
+      content: '""',
+      borderRadius: 4,
+      background: `${Color.Blue300}`,
+
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      zIndex: -1,
+      position: 'absolute',
     },
   };
 }
@@ -33,32 +32,38 @@ export const useDateRangePickerStyles = makeStyles(
   {
     day: { zIndex: 1 },
 
-    firstDayOfRange: makeRangeStyles('first'),
-    lastDayOfRange: makeRangeStyles('last'),
+    monthStart: {},
+    monthEnd: {},
+
+    rangeStart: makeRangeStyles('first'),
+    rangeEnd: makeRangeStyles('last'),
 
     selected: {
-      '&:not(.SuperDispatchCalendar-outside)': {
-        color: Color.Blue500,
+      color: Color.Blue500,
+      backgroundColor: 'transparent',
 
-        '&:before': {
-          content: '""',
-          position: 'absolute',
-          zIndex: -1,
-          top: 0,
-          left: -1,
-          right: -1,
-          bottom: 0,
-          background: Color.Blue50,
-        },
-
-        '&:first-of-type:before': {
-          borderRadius: '4px 0 0 4px',
-        },
-
-        '&:last-of-type:before': {
-          borderRadius: '0 4px 4px 0',
-        },
+      '&:before': {
+        content: '""',
+        top: 0,
+        left: -1,
+        right: -1,
+        bottom: 0,
+        zIndex: -1,
+        position: 'absolute',
+        background: Color.Blue50,
       },
+
+      '&:first-child, &$monthStart': {
+        '&:before': { borderRadius: '4px 0 0 4px' },
+      },
+
+      '&:last-child, &$monthEnd': {
+        '&:before': { borderRadius: '0 4px 4px 0' },
+      },
+    },
+
+    outside: {
+      visibility: 'hidden',
     },
   },
   { name: 'SuperDispatchDateRangePicker' },
