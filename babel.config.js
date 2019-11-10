17a15dedf69d2f5151f21d27ae2ed14388f7cc20
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2019-11-10 17:38:23 +0500

    docs: Add share in CodeSandbox button. (#32)

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@
 module.exports = {
   presets: ['@babel/typescript', '@babel/react'],
   plugins: [
+    './tools/babel-plugin-condesandbox-url',
     ['@babel/plugin-proposal-optional-chaining', { loose: true }],
     ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: true }],
   ],
