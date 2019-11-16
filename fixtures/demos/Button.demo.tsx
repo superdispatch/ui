770a74c71172cd36ca04163ecb7142ffdff8acfe
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2019-11-16 19:57:39 +0500

    feat(ui): Adapt `Button` sizes in mobile. (#33)

--- fixtures/demos/Button.demo.tsx
+++ fixtures/demos/Button.demo.tsx
@@ -12,36 +12,30 @@ import {
 import { MoreHoriz, Save, Send } from '@material-ui/icons';
 import { Button, ButtonProps } from '@superdispatch/ui';
 import { startCase } from 'lodash';
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, MouseEvent, useEffect, useState } from 'react';
 
 type State = 'stale' | 'disabled' | 'active' | 'loading';
 const states: State[] = ['stale', 'disabled', 'active', 'loading'];
 const colors: Array<ButtonProps['color']> = ['primary', 'success', 'error'];
 
 const sizes: Array<ButtonProps['size']> = ['small', 'medium', 'large'];
-const variants: Array<ButtonProps['variant']> = ['text', 'contained', 'outlined'];
+const variants: Array<ButtonProps['variant']> = ['text', 'outlined', 'contained'];
 
 export default function ButtonDemo() {
-  const [state, setState] = useState<State>('stale');
+  const [buttonStateMap, setButtonStateMap] = useState<Map<string, State>>(new Map());
+  const [globalState, setGlobalState] = useState<State>('stale');
   const [color, setColor] = useState<ButtonProps['color']>('primary');
-  const [lastClicked, setLastClicked] = useState(0);
   const [hasEndIcon, setHasEndIcon] = useState(false);
   const [hasStartIcon, setHasStartIcon] = useState(false);
 
   useEffect(() => {
-    if (!lastClicked) {
+    if (buttonStateMap.size === 0) {
       return;
     }
 
-    setState('loading');
-
-    const timeout = setTimeout(() => {
-      setState('stale');
-      setLastClicked(0);
-    }, 1000);
-
+    const timeout = setTimeout(() => setButtonStateMap(new Map()), 1000);
     return () => clearTimeout(timeout);
-  }, [lastClicked]);
+  }, [buttonStateMap]);
 
   return (
     <>
@@ -73,9 +67,9 @@ export default function ButtonDemo() {
               <FormLabel component="legend">State</FormLabel>
               <RadioGroup
                 row={true}
-                value={state}
+                value={globalState}
                 name="state"
-                onChange={(_, value) => setState(value as State)}
+                onChange={(_, value) => setGlobalState(value as State)}
               >
                 {states.map(x => (
                   <FormControlLabel key={x} value={x} control={<Radio />} label={startCase(x)} />
@@ -106,41 +100,49 @@ export default function ButtonDemo() {
         <Grid container={true} spacing={1}>
           {variants.map(variant => (
             <Fragment key={variant}>
-              {sizes.map(size => (
-                <Grid item={true} key={size} sm={4} xs={12}>
-                  <Grid container={true} spacing={1}>
-                    <Grid item={true}>
-                      <Button
-                        size={size}
-                        color={color}
-                        variant={variant}
-                        disabled={state === 'disabled'}
-                        isActive={state === 'active'}
-                        isLoading={state === 'loading'}
-                        startIcon={hasStartIcon && <Save />}
-                        endIcon={hasEndIcon && <Send />}
-                        onClick={() => setLastClicked(Date.now())}
-                      >
-                        Button
-                      </Button>
-                    </Grid>
+              {sizes.map(size => {
+                const buttonKey = `${variant}-${size}`;
+                const buttonState = buttonStateMap.get(buttonKey) || globalState;
+                const props: ButtonProps = {
+                  size,
+                  color,
+                  variant,
+                  disabled: buttonState === 'disabled',
+                  isActive: buttonState === 'active',
+                  isLoading: buttonState === 'loading',
+                  onClick: (event: MouseEvent) => {
+                    if (event.shiftKey) {
+                      return;
+                    }
 
-                    <Grid item={true}>
-                      <Button
-                        size={size}
-                        color={color}
-                        variant={variant}
-                        disabled={state === 'disabled'}
-                        isActive={state === 'active'}
-                        isLoading={state === 'loading'}
-                        onClick={() => setLastClicked(Date.now())}
-                      >
-                        <MoreHoriz />
-                      </Button>
+                    setButtonStateMap(prev =>
+                      new Map(prev).set(buttonKey, event.altKey ? 'disabled' : 'loading'),
+                    );
+                  },
+                };
+
+                return (
+                  <Grid item={true} key={size} sm={4} xs={12}>
+                    <Grid container={true} spacing={1}>
+                      <Grid item={true}>
+                        <Button
+                          {...props}
+                          startIcon={hasStartIcon && <Save />}
+                          endIcon={hasEndIcon && <Send />}
+                        >
+                          Button
+                        </Button>
+                      </Grid>
+
+                      <Grid item={true}>
+                        <Button {...props}>
+                          <MoreHoriz />
+                        </Button>
+                      </Grid>
                     </Grid>
                   </Grid>
-                </Grid>
-              ))}
+                );
+              })}
             </Fragment>
           ))}
         </Grid>
