fa0de9965c9deba4503aea975e7c48d050abc7e2
Umidbek Karimov <uma.karimov@gmail.com>
2019-11-16 20:11:28 +0500

    refactor(ui): Move `Tag` styles in to it's component.

--- packages/ui/src/tag/Tag.tsx
+++ packages/ui/src/tag/Tag.tsx
@@ -1,11 +1,71 @@
-import { Typography } from '@material-ui/core';
+import { makeStyles, Theme, Typography } from '@material-ui/core';
 import { TypographyProps } from '@material-ui/core/Typography';
 import { ClassNameMap } from '@material-ui/styles/withStyles';
 import cx from 'clsx';
 import React, { forwardRef } from 'react';
 
-import { ColorVariant } from '../theme/Color';
-import { TagClassKey, useTagStyles } from './TagStyles';
+import { Color, ColorVariant } from '../theme/Color';
+
+export type TagClassKey =
+  | 'root'
+  | 'colorGrey'
+  | 'colorBlue'
+  | 'colorGreen'
+  | 'colorPurple'
+  | 'colorRed'
+  | 'colorTeal'
+  | 'colorYellow'
+  | 'variantSubtle'
+  | 'variantBold';
+
+const useStyles = makeStyles<Theme, {}, TagClassKey>(
+  theme => ({
+    root: {
+      maxWidth: '100%',
+      alignItems: 'center',
+      display: 'inline-flex',
+      borderRadius: theme.spacing(0.5),
+      padding: theme.spacing(0, 0.5),
+    },
+
+    variantSubtle: {
+      '&$colorGrey': { color: Color.Grey300, backgroundColor: Color.Silver200 },
+      '&$colorBlue': { color: Color.Blue500, backgroundColor: Color.Blue50 },
+      '&$colorGreen': { color: Color.Green500, backgroundColor: Color.Green50 },
+      '&$colorPurple': {
+        color: Color.Purple500,
+        backgroundColor: Color.Purple50,
+      },
+      '&$colorRed': { color: Color.Red500, backgroundColor: Color.Red50 },
+      '&$colorTeal': { color: Color.Teal500, backgroundColor: Color.Teal50 },
+      '&$colorYellow': {
+        color: Color.Yellow500,
+        backgroundColor: Color.Yellow50,
+      },
+    },
+
+    variantBold: {
+      color: Color.White,
+
+      '&$colorGrey': { backgroundColor: Color.Grey300 },
+      '&$colorBlue': { backgroundColor: Color.Blue500 },
+      '&$colorGreen': { backgroundColor: Color.Green500 },
+      '&$colorPurple': { backgroundColor: Color.Purple500 },
+      '&$colorRed': { backgroundColor: Color.Red500 },
+      '&$colorTeal': { backgroundColor: Color.Teal500 },
+      '&$colorYellow': { backgroundColor: Color.Yellow500 },
+    },
+
+    colorGrey: {},
+    colorBlue: {},
+    colorGreen: {},
+    colorPurple: {},
+    colorRed: {},
+    colorTeal: {},
+    colorYellow: {},
+  }),
+  { name: 'SuperDispatchTag' },
+);
 
 export interface TagProps extends Omit<TypographyProps, 'classes' | 'color' | 'variant'> {
   classes?: ClassNameMap<TagClassKey>;
@@ -19,7 +79,7 @@ export const Tag = forwardRef<HTMLSpanElement, TagProps>(
     { color, variant, children, classes, className, noWrap = true, component = 'div', ...props },
     ref,
   ) => {
-    const styles = useTagStyles({ classes });
+    const styles = useStyles({ classes });
 
     return (
       <Typography
