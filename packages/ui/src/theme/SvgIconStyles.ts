3df2a948d62d97d0852a676ae9559057439cac03
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2019-11-21 17:03:17 +0500

    refactor(ui): Expose icon style helpers.

--- packages/ui/src/theme/SvgIconStyles.ts
+++ packages/ui/src/theme/SvgIconStyles.ts
@@ -1,14 +1,32 @@
 import { Theme } from '@material-ui/core';
+import { IconProps } from '@material-ui/core/Icon';
 
 import { Color } from './Color';
+
+export function iconSizeVariant(size?: IconProps['fontSize'], isMobile = false): string {
+  switch (size) {
+    case 'small':
+      return `${isMobile ? 24 : 16}px`;
+    case 'large':
+      return '32px';
+    default:
+      return `${isMobile ? 32 : 24}px`;
+  }
+}
 
 export function applySvgIconStyles(theme: Theme) {
   theme.overrides = theme.overrides || {};
 
   theme.overrides.MuiSvgIcon = {
-    root: { fontSize: 24, [theme.breakpoints.only('xs')]: { fontSize: 32 } },
-    fontSizeSmall: { fontSize: 16, [theme.breakpoints.only('xs')]: { fontSize: 24 } },
-    fontSizeLarge: { fontSize: 32 },
+    root: {
+      fontSize: iconSizeVariant('default'),
+      [theme.breakpoints.only('xs')]: { fontSize: iconSizeVariant('default', true) },
+    },
+    fontSizeSmall: {
+      fontSize: iconSizeVariant('small'),
+      [theme.breakpoints.only('xs')]: { fontSize: iconSizeVariant('small', true) },
+    },
+    fontSizeLarge: { fontSize: iconSizeVariant('large') },
     colorAction: { color: Color.Grey100 },
   };
 }
