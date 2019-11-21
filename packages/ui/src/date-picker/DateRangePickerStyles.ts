b3417a90633b2d57e45182650befd45845d26434
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2019-11-21 20:17:57 +0500

    refactor(ui): Use `theme.spacing` in `DateRangePickerStyles`.

--- packages/ui/src/date-picker/DateRangePickerStyles.ts
+++ packages/ui/src/date-picker/DateRangePickerStyles.ts
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/styles';
 import { Color } from '../theme/Color';
 
 export const useDateRangePickerStyles = makeStyles<Theme>(
-  {
+  theme => ({
     outside: {},
     disabled: {},
     selected: {},
@@ -13,33 +13,18 @@ export const useDateRangePickerStyles = makeStyles<Theme>(
 
     day: {
       '&$selected:not($outside)': {
-        '&$rangeStart:before': {
-          left: '4px',
-        },
-
-        '&$rangeEnd:before': {
-          right: '4px',
-        },
-
+        '&$rangeStart:before': { left: theme.spacing(0.5) },
+        '&$rangeEnd:before': { right: theme.spacing(0.5) },
         '&:not($rangeStart):not($rangeEnd)': {
-          '&:after': {
-            backgroundColor: 'transparent',
-          },
-
-          '&$disabled': {
-            '&:before': {
-              backgroundColor: Color.Silver100,
-            },
-          },
+          '&:after': { backgroundColor: 'transparent' },
+          '&$disabled': { '&:before': { backgroundColor: Color.Silver100 } },
           '&:not($disabled)': {
             color: Color.Blue500,
-            '&:before': {
-              backgroundColor: Color.Blue50,
-            },
+            '&:before': { backgroundColor: Color.Blue50 },
           },
         },
       },
     },
-  },
+  }),
   { name: 'SuperDispatchDateRangePicker' },
 );
