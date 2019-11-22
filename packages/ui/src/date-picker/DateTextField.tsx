175a06b9fd609ed13e0aec670f1b61575fbf0d29
doniyor2109 <doniyor2109@gmail.com>
2019-11-22 11:38:11 +0500

    feat(ui): Use IconButton for DateTextField

--- packages/ui/src/date-picker/DateTextField.tsx
+++ packages/ui/src/date-picker/DateTextField.tsx
@@ -1,12 +1,12 @@
-import { InputAdornment, SvgIcon, TextField } from '@material-ui/core';
-import { SvgIconProps } from '@material-ui/core/SvgIcon';
+import { IconButton, InputAdornment, SvgIcon, TextField } from '@material-ui/core';
+import { IconButtonProps } from '@material-ui/core/IconButton';
 import { OutlinedTextFieldProps } from '@material-ui/core/TextField';
 import { mdiCalendarMonth } from '@mdi/js';
 import React, { forwardRef } from 'react';
 
 export interface DateTextFieldProps extends Omit<OutlinedTextFieldProps, 'value' | 'variant'> {
   value: string;
-  IconProps?: SvgIconProps;
+  IconProps?: IconButtonProps;
 }
 
 export const DateTextField = forwardRef<HTMLDivElement, DateTextFieldProps>(
@@ -18,9 +18,11 @@ export const DateTextField = forwardRef<HTMLDivElement, DateTextFieldProps>(
       InputProps={{
         endAdornment: (
           <InputAdornment position="end">
-            <SvgIcon color="action" {...IconProps}>
-              <path d={mdiCalendarMonth} />
-            </SvgIcon>
+            <IconButton size="small" {...IconProps}>
+              <SvgIcon color="action">
+                <path d={mdiCalendarMonth} />
+              </SvgIcon>
+            </IconButton>
           </InputAdornment>
         ),
         ...props.InputProps,
