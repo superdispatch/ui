d1ebb48feff76a5dd0fd3ed4c939ce74d990944f
Umidbek Karimov <uma.karimov@gmail.com>
2019-12-16 16:16:37 +0500

    chore: Bump dependencies.

--- packages/ui/src/button/Button.tsx
+++ packages/ui/src/button/Button.tsx
@@ -163,6 +163,7 @@ const useStyles = makeStyles<Theme, {}, ButtonClassKey>(
     containedSizeSmall: {},
     containedSizeLarge: {},
 
+    disableElevation: {},
     focusVisible: {},
     disabled: {},
     active: {},
