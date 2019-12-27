55bfdeff8632fd66cdc25a2e48ecb256484f8a06
Umidbek Karimov <uma.karimov@gmail.com>
2019-12-27 11:35:25 +0500

    [WIP]

--- packages/ui/src/theme/styles/CheckboxStyles.tsx
+++ packages/ui/src/theme/styles/CheckboxStyles.tsx
@@ -14,7 +14,7 @@ export function applyCheckboxStyles(theme: SuperDispatchTheme) {
           height="17"
           x="3.5"
           y="3.5"
-          fill="#fff"
+          fill={Color.White}
           stroke="currentColor"
           rx="1.5"
         />
@@ -24,7 +24,7 @@ export function applyCheckboxStyles(theme: SuperDispatchTheme) {
       <SvgIcon>
         <rect width="18" height="18" x="3" y="3" fill="currentColor" rx="2" />
         <path
-          fill="#fff"
+          fill={Color.White}
           d="M15.73 8l-.63.63c-1.43 1.43-2.94 3.05-4.37 4.5l-1.9-1.57-.7-.57L7 12.38l.7.57 2.53 2.09.63.52.58-.58c1.6-1.62 3.35-3.5 4.93-5.08l.63-.63L15.73 8z"
         />
       </SvgIcon>
@@ -32,7 +32,7 @@ export function applyCheckboxStyles(theme: SuperDispatchTheme) {
     indeterminateIcon: (
       <SvgIcon>
         <rect width="18" height="18" x="3" y="3" fill="currentIcon" rx="2" />
-        <path fill="#fff" d="M7 11h10v2H7v-2z" />
+        <path fill={Color.White} d="M7 11h10v2H7v-2z" />
       </SvgIcon>
     ),
   };
