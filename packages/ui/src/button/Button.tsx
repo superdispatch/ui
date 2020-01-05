af63720f6072d5ae24b1b998c5a945fa06025118
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-01-05 17:14:18 +0500

    refactor(ui): Explicitly set type of `Button` component.

--- packages/ui/src/button/Button.tsx
+++ packages/ui/src/button/Button.tsx
@@ -1,8 +1,14 @@
 import { Button as MaterialButton, CircularProgress } from '@material-ui/core';
 import { ButtonProps as MuiButtonProps } from '@material-ui/core/Button';
-import React, { forwardRef } from 'react';
+import React, {
+  forwardRef,
+  ForwardRefExoticComponent,
+  RefAttributes,
+} from 'react';
 
-export interface ButtonProps extends Omit<MuiButtonProps, 'color' | 'variant'> {
+export interface ButtonProps
+  extends RefAttributes<HTMLButtonElement>,
+    Omit<MuiButtonProps, 'color' | 'variant'> {
   rel?: string;
   target?: string;
   isActive?: boolean;
@@ -11,26 +17,27 @@ export interface ButtonProps extends Omit<MuiButtonProps, 'color' | 'variant'> {
   variant: 'text' | 'outlined' | 'contained';
 }
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ size, color, children, disabled, isActive, isLoading, ...props }, ref) => (
-    <MaterialButton
-      {...props}
-      ref={ref}
-      size={size}
-      disabled={disabled || isLoading}
-      data-color={color}
-      color={color === 'primary' ? color : undefined}
-      aria-busy={isLoading ? 'true' : 'false'}
-      aria-expanded={isActive ? 'true' : 'false'}
-    >
-      {!isLoading ? (
-        children
-      ) : (
-        <>
-          {children}
-          <CircularProgress size="1em" color="inherit" />
-        </>
-      )}
-    </MaterialButton>
-  ),
-);
+export const Button: ForwardRefExoticComponent<ButtonProps> = forwardRef<
+  HTMLButtonElement,
+  ButtonProps
+>(({ size, color, children, disabled, isActive, isLoading, ...props }, ref) => (
+  <MaterialButton
+    {...props}
+    ref={ref}
+    size={size}
+    disabled={disabled || isLoading}
+    data-color={color}
+    color={color === 'primary' ? color : undefined}
+    aria-busy={isLoading ? 'true' : 'false'}
+    aria-expanded={isActive ? 'true' : 'false'}
+  >
+    {!isLoading ? (
+      children
+    ) : (
+      <>
+        {children}
+        <CircularProgress size="1em" color="inherit" />
+      </>
+    )}
+  </MaterialButton>
+));
