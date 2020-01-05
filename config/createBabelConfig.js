c0ceac7f9e5ac4eed6bbd876d8b4e702dfe36923
Umidbek Karimov <uma.karimov@gmail.com>
2020-01-05 17:27:17 +0500

    chore: Use `@superdispatch/babel-preset`.

--- config/createBabelConfig.js
+++ config/createBabelConfig.js
@@ -7,20 +7,12 @@ module.exports = ({ isDocs = false } = {}) => api => {
 
   return {
     presets: [
-      '@babel/react',
-      '@babel/typescript',
-      isTest && [
-        '@babel/preset-env',
-        { modules: 'cjs', targets: { node: true } },
-      ],
-    ].filter(Boolean),
+      ['@superdispatch/babel-preset', { loose: true, targets: 'esmodules' }],
+    ],
     plugins: [
       require.resolve('../tools/babel-plugin-inject-display-name'),
       isDocs && !isTest && require.resolve('../tools/babel-plugin-csb'),
       ['babel-plugin-optimize-clsx', { libraries: ['clsx'] }],
-      ['@babel/plugin-transform-runtime', { useESModules: false }],
-      ['@babel/plugin-proposal-optional-chaining', { loose: true }],
-      ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: true }],
     ].filter(Boolean),
   };
 };
