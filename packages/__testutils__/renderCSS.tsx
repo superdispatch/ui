b36ae4d1662fd2690cecf66f588b7d7437f5e4b0
Umidbek Karimov <uma.karimov@gmail.com>
2020-01-23 11:12:00 +0500

    WIP

--- packages/__testutils__/renderCSS.tsx
+++ packages/__testutils__/renderCSS.tsx
@@ -1,3 +1,44 @@
-import { ReactElement } from 'react';
+import { Color, ThemeProvider } from '@superdispatch/ui';
+import { render } from '@testing-library/react';
+import { SheetsRegistry } from 'jss';
+import { format } from 'prettier';
+import React, { ComponentType, ReactElement } from 'react';
 
-export function renderCSS(samples: ReactElement[]): string[] {}
+const colors = new Map<string, string>(
+  Object.entries(Color).map(([k, v]) => [v.toLowerCase(), `Color.${k}`]),
+);
+const colorRegExp = new RegExp(
+  [...colors.keys()]
+    .map(x => x.replace('(', '\\(').replace(')', '\\)'))
+    .join('|'),
+  'g',
+);
+
+let sheetsRegistry: SheetsRegistry;
+const Wrapper: ComponentType = ({ children }) => (
+  <ThemeProvider sheetsRegistry={sheetsRegistry}>{children}</ThemeProvider>
+);
+
+function getCSS(baseCSS?: string) {
+  const css = format(sheetsRegistry.toString(), {
+    parser: 'css',
+  })
+    .replace(colorRegExp, color => colors.get(color) as string)
+    .replace(/"/g, "'");
+
+  return !baseCSS ? css : css.replace(baseCSS, '');
+}
+
+beforeEach(() => {
+  sheetsRegistry = new SheetsRegistry();
+});
+
+export function renderCSS(ui: ReactElement): string {
+  const { rerender } = render(<div />, { wrapper: Wrapper });
+
+  const baseCSS = getCSS();
+
+  rerender(ui);
+
+  return getCSS(baseCSS);
+}
