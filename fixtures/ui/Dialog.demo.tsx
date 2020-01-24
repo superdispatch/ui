cb418d3b5ef5b9206f98cb889aca89a18bb2eb23
Umidbek Karimov <uma.karimov@gmail.com>
2020-01-24 17:03:20 +0500

    feat(ui): Use `theme.typography.h3` styles for `DialogTitle`.

--- fixtures/ui/Dialog.demo.tsx
+++ fixtures/ui/Dialog.demo.tsx
@@ -4,7 +4,6 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
-  Typography,
 } from '@material-ui/core';
 import { Button } from '@superdispatch/ui';
 import React, { useState } from 'react';
@@ -23,9 +22,7 @@ export default function DialogDemo() {
       </Button>
 
       <Dialog open={isOpen}>
-        <DialogTitle>
-          <Typography variant="h3">Use Google’s location service?</Typography>
-        </DialogTitle>
+        <DialogTitle>Use Google’s location service?</DialogTitle>
         <DialogContent>
           Let Google help apps determine location. This means sending anonymous
           location data to Google, even when no apps are running.
