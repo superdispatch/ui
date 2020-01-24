b78bf5524ea3d6065749e9d03a15ad494d70906d
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-01-24 17:03:20 +0500

    feat(ui): Use `theme.typography.h3` styles for `DialogTitle`.

--- packages/ui/src/dialog/DialogStyles.ts
+++ packages/ui/src/dialog/DialogStyles.ts
@@ -3,6 +3,10 @@ import { SuperDispatchTheme } from '../theme/ThemeProvider';
 export function applyDialogStyles(theme: SuperDispatchTheme) {
   theme.props.MuiDialogTitle = { disableTypography: true };
 
+  theme.overrides.MuiDialogTitle = {
+    root: { ...theme.typography.h3 },
+  };
+
   theme.overrides.MuiDialogContent = {
     root: { padding: theme.spacing(0, 3) },
   };
