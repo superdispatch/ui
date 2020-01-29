60e83ecddab84d21e0bd50cf55c667256404c095
Umidbek Karimov <uma.karimov@gmail.com>
2020-01-29 11:15:41 +0500

    docs: Move date components to `Dates` category.

---
+++ packages/__fixtures__/Dates.fixture.ts
@@ -0,0 +1,13 @@
+import CalendarDemo from './dates/Calendar.demo';
+import DateFieldDemo from './dates/DateField.demo';
+import DatePickerDemo from './dates/DatePicker.demo';
+import DateRangeFieldDemo from './dates/DateRangeField.demo';
+import DateRangePickerDemo from './dates/DateRangePicker.demo';
+
+export default {
+  Calendar: CalendarDemo,
+  'Date Field': DateFieldDemo,
+  'Date Range Field': DateRangeFieldDemo,
+  'Date Picker': DatePickerDemo,
+  'Date Range Picker': DateRangePickerDemo,
+};
--- packages/__fixtures__/Inputs.fixture.ts
+++ packages/__fixtures__/Inputs.fixture.ts
@@ -1,11 +1,6 @@
 import ButtonDemo from './ui/Button.demo';
-import CalendarDemo from './ui/Calendar.demo';
 import CardButtonDemo from './ui/CardButton.demo';
 import CheckboxDemo from './ui/Checkbox.demo';
-import DateFieldDemo from './ui/DateField.demo';
-import DatePickerDemo from './ui/DatePicker.demo';
-import DateRangeFieldDemo from './ui/DateRangeField.demo';
-import DateRangePickerDemo from './ui/DateRangePicker.demo';
 import IconButtonDemo from './ui/IconButton.demo';
 import RadioDemo from './ui/Radio.demo';
 import SwitchDemo from './ui/Switch.demo';
@@ -13,14 +8,9 @@ import TextFieldDemo from './ui/TextField.demo';
 
 export default {
   Button: ButtonDemo,
-  Calendar: CalendarDemo,
   'Card Button': CardButtonDemo,
   Checkbox: CheckboxDemo,
   IconButton: IconButtonDemo,
-  'Date Field': DateFieldDemo,
-  'Date Range Field': DateRangeFieldDemo,
-  'Date Picker': DatePickerDemo,
-  'Date Range Picker': DateRangePickerDemo,
   Radio: RadioDemo,
   Switch: SwitchDemo,
   'Text Field': TextFieldDemo,
