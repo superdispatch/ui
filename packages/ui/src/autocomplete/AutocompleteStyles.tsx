7ea24da31851709bd4a6557c233318d08912cf15
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-02-24 11:06:10 +0500

    refactor(ui): Move `Autocomplete` overrides to `AutocompleteStyles`.

---
+++ packages/ui/src/autocomplete/AutocompleteStyles.tsx
@@ -0,0 +1,80 @@
+import { SvgIcon } from '@material-ui/core';
+import React from 'react';
+
+import { Color } from '../theme/Color';
+import { SuperDispatchTheme } from '../theme/ThemeProvider';
+
+export function applyAutocompleteStyles(theme: SuperDispatchTheme) {
+  // TODO: Remove `Object.assign` after official release of `Autocomplete`.
+
+  Object.assign(theme.props, {
+    MuiAutocomplete: {
+      closeIcon: (
+        <SvgIcon>
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            fill="currentColor"
+            d="M13.239 12L17 8.239 15.761 7 12 10.761 8.239 7 7 8.239 10.761 12 7 15.761 8.239 17 12 13.239 15.761 17 17 15.761 13.239 12z"
+          />
+        </SvgIcon>
+      ),
+    },
+  });
+
+  Object.assign(theme.overrides, {
+    MuiAutocomplete: {
+      paper: { ...theme.typography.body2 },
+      tag: {
+        margin: theme.spacing(0.5),
+
+        [theme.breakpoints.up('sm')]: {
+          margin: theme.spacing(0.25),
+        },
+      },
+      endAdornment: {
+        top: 0,
+        bottom: 0,
+        display: 'flex',
+        alignItems: 'center',
+      },
+      popupIndicator: {
+        '& .MuiSvgIcon-root': {
+          fontSize: theme.spacing(3),
+        },
+      },
+      clearIndicator: {
+        '& .MuiSvgIcon-root': {
+          color: Color.Grey100,
+          fontSize: theme.spacing(3),
+
+          [theme.breakpoints.up('sm')]: {
+            fontSize: theme.spacing(2),
+          },
+        },
+      },
+      inputRoot: {
+        '&[class*="MuiOutlinedInput-root"]': {
+          padding: theme.spacing(0.75, 1),
+
+          '& $input': {
+            padding: theme.spacing(0.5),
+            minWidth: theme.spacing(12),
+          },
+
+          '& $input:first-child': {
+            paddingLeft: undefined,
+          },
+
+          [theme.breakpoints.up('sm')]: {
+            padding: theme.spacing(0.5, 0.75),
+
+            '& $input': {
+              padding: theme.spacing(0.25),
+            },
+          },
+        },
+      },
+    },
+  });
+}
