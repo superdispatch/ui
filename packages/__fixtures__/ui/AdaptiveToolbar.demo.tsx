7bcc78b7d4599da56b442c93ead08db049b89504
Umidbek Karimov <uma.karimov@gmail.com>
2020-03-03 19:49:26 +0500

    WIP

--- packages/__fixtures__/ui/AdaptiveToolbar.demo.tsx
+++ packages/__fixtures__/ui/AdaptiveToolbar.demo.tsx
@@ -30,63 +30,19 @@ export function AdaptiveToolbarDemo() {
       <AppBar style={{ maxWidth: width }}>
         <AdaptiveToolbar
           items={[
-            {
-              key: 'send_offer',
-              label: 'Send Offer',
-              onClick: () => {
-                addSnackbar('Send Offer');
-              },
-            },
-            {
-              key: 'post_to_slb',
-              label: 'Post to SLB',
-              onClick: () => {
-                addSnackbar('Post to SLB');
-              },
-            },
-            {
-              key: 'change_date_ranges',
-              label: 'Change Date Ranges',
-              onClick: () => {
-                addSnackbar('Change Date Ranges');
-              },
-            },
-            {
-              key: 'increase_price_by',
-              label: 'Increase Price By',
-              onClick: () => {
-                addSnackbar('Increase Price By');
-              },
-            },
-            {
-              key: 'mark_as_accepted',
-              label: 'Mark as Accepted',
-              onClick: () => {
-                addSnackbar('Mark as Accepted');
-              },
-            },
-            {
-              key: 'send_customer_invoice',
-              label: 'Send Customer Invoice',
-              onClick: () => {
-                addSnackbar('Send Customer Invoice');
-              },
-            },
-            {
-              key: 'archive',
-              label: 'Archive',
-              onClick: () => {
-                addSnackbar('Archive');
-              },
-            },
-            {
-              key: 'delete',
-              label: 'Delete',
-              onClick: () => {
-                addSnackbar('Delete');
-              },
-            },
-          ]}
+            'Send Offer',
+            'Post to SLB',
+            'Change Date Ranges',
+            'Increase Price By',
+            'Mark as Accepted',
+            'Send Customer Invoice',
+            'Archive',
+            'Delete',
+          ].map(label => ({
+            label,
+            key: label,
+            onClick: () => addSnackbar(label),
+          }))}
         />
       </AppBar>
     </GridStack>
