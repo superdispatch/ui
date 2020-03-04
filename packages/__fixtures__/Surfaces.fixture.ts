bdb8c9a09f278853b7ed03cf8045c1d0eabdab41
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-03-04 10:48:39 +0500

    feat(ui): Implement `AdaptiveToolbar`. (#59)

--- packages/__fixtures__/Surfaces.fixture.ts
+++ packages/__fixtures__/Surfaces.fixture.ts
@@ -1,8 +1,10 @@
+import { AdaptiveToolbarDemo } from './ui/AdaptiveToolbar.demo';
 import AppBarDemo from './ui/AppBar.demo';
 import CardDemo from './ui/Card.demo';
 import PaperDemo from './ui/Paper.demo';
 
 export default {
+  'Adaptive Toolbar': AdaptiveToolbarDemo,
   'App Bar': AppBarDemo,
   Card: CardDemo,
   Paper: PaperDemo,
---
+++ packages/ui/src/adaptive-toolbar/AdaptiveToolbar.tsx
@@ -0,0 +1,173 @@
+import { ResizeObserver } from '@juggle/resize-observer';
+import {
+  Grid,
+  Menu,
+  MenuItem,
+  Toolbar,
+  ToolbarProps,
+  Typography,
+} from '@material-ui/core';
+import { MoreHoriz } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/styles';
+import React, {
+  forwardRef,
+  ForwardRefExoticComponent,
+  Key,
+  ReactNode,
+  RefAttributes,
+  useRef,
+  useState,
+} from 'react';
+import { useEventCallback, useIsomorphicLayoutEffect } from 'utility-hooks';
+
+import { Button } from '../button/Button';
+
+function useResizeObserver<T extends HTMLElement>(
+  node: null | undefined | T,
+  callback: (node: T) => void,
+): void {
+  const cb = useEventCallback(callback);
+
+  useIsomorphicLayoutEffect(() => {
+    if (!node) {
+      return;
+    }
+
+    const resizeObserver = new ResizeObserver(() => {
+      cb(node);
+    });
+
+    resizeObserver.observe(node);
+
+    cb(node);
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [cb, node]);
+}
+
+const useStyles = makeStyles(
+  { actions: { overflow: 'hidden' } },
+  { name: 'SuperDispatchAdaptiveToolbar' },
+);
+
+export interface AdaptiveToolbarItem {
+  key: Key;
+  label: ReactNode;
+  onClick: () => void;
+}
+
+export interface AdaptiveToolbarProps
+  extends RefAttributes<unknown>,
+    Omit<ToolbarProps, 'children'> {
+  items: AdaptiveToolbarItem[];
+}
+
+export const AdaptiveToolbar: ForwardRefExoticComponent<AdaptiveToolbarProps> = forwardRef(
+  ({ items, ...props }, ref) => {
+    const styles = useStyles();
+
+    const itemNodes = useRef<Array<null | HTMLElement>>([]);
+    const optionsButtonRef = useRef<HTMLDivElement>(null);
+    const [firstHiddenIdx, setFirstHiddenIdx] = useState(-1);
+
+    const menuItems = firstHiddenIdx === -1 ? [] : items.slice(firstHiddenIdx);
+    const [menuButtonNode, setMenuButtonRef] = useState<HTMLElement>();
+
+    const [rootNode, setRootNode] = useState<null | HTMLDivElement>(null);
+
+    useResizeObserver(rootNode, () => {
+      const rootRect = (rootNode as HTMLElement).getBoundingClientRect();
+      const rootWidth = rootRect.left + rootRect.width;
+
+      const optionsButtonRect = optionsButtonRef.current?.getBoundingClientRect();
+      const optionsButtonWidth = optionsButtonRect?.width || 0;
+      const maxRightPosition = rootWidth - optionsButtonWidth;
+
+      const mountedNodes = itemNodes.current.filter(
+        (x): x is HTMLDivElement => x != null,
+      );
+      const hiddenIdx = mountedNodes.findIndex((itemNode, idx) => {
+        itemNode.removeAttribute('hidden');
+
+        const itemRect = itemNode.getBoundingClientRect();
+        const itemRightPosition = itemRect.left + itemRect.width;
+
+        // Ignore options button width when checking last item.
+        if (idx === mountedNodes.length - 1) {
+          return itemRightPosition > rootWidth;
+        }
+
+        return itemRightPosition > maxRightPosition;
+      });
+
+      if (hiddenIdx !== -1) {
+        mountedNodes.slice(hiddenIdx).forEach(itemNode => {
+          itemNode.setAttribute('hidden', 'true');
+        });
+      }
+
+      setFirstHiddenIdx(hiddenIdx);
+    });
+
+    return (
+      <Toolbar {...props} ref={ref}>
+        <Grid container={true} spacing={1} wrap="nowrap" ref={setRootNode}>
+          <Grid item={true} className={styles.actions}>
+            <Grid container={true} spacing={1} wrap="nowrap" component="div">
+              {items.map((item, idx) => (
+                <Grid
+                  key={item.key}
+                  item={true}
+                  ref={node => {
+                    itemNodes.current[idx] = node;
+                  }}
+                >
+                  <Button type="button" onClick={item.onClick}>
+                    <Typography noWrap={true} variant="inherit">
+                      {item.label}
+                    </Typography>
+                  </Button>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+
+          {menuItems.length > 0 && (
+            <Grid item={true} ref={optionsButtonRef} component="div">
+              <Button
+                type="button"
+                onClick={({ currentTarget }) => {
+                  setMenuButtonRef(currentTarget);
+                }}
+              >
+                <MoreHoriz />
+              </Button>
+
+              <Menu
+                open={!!menuButtonNode}
+                anchorEl={menuButtonNode}
+                onClose={() => {
+                  setMenuButtonRef(undefined);
+                }}
+              >
+                {menuItems.map(item => (
+                  <MenuItem
+                    key={item.key}
+                    onClick={() => {
+                      item.onClick();
+                      setMenuButtonRef(undefined);
+                    }}
+                  >
+                    {item.label}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Grid>
+          )}
+        </Grid>
+      </Toolbar>
+    );
+  },
+);
