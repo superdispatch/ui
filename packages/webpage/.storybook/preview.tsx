5dfc58bfc2576f28c69106bb0fd6a1325218ae6c
Umidbek Karimov <uma.karimov@gmail.com>
2020-03-05 20:06:22 +0500

    WIP

--- packages/webpage/.storybook/preview.tsx
+++ packages/webpage/.storybook/preview.tsx
@@ -1,10 +1,57 @@
 import { addDecorator } from '@storybook/react';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import { ThemeProvider } from '@superdispatch/ui';
-import { Box } from '@material-ui/core';
+import { Box, Fab, CircularProgress } from '@material-ui/core';
+import { StoryContext } from '@storybook/addons';
+import { get } from 'lodash';
+import { usePromise } from 'utility-hooks';
+import ky from 'ky';
+import { Edit } from '@material-ui/icons';
+
+function ThemeDecorator({ children, ...ctx }: PropsWithChildren<StoryContext>) {
+  const Component = ctx.hooks?.mountedDecorators?.values().next().value;
+  const csbParams = get(Component, 'csbParams');
+  const sandbox = usePromise<{ sandbox_id: string }>(
+    ({ abortSignal }) =>
+      ky('https://codesandbox.io/api/v1/sandboxes/define', {
+        signal: abortSignal,
+        searchParams: { json: 1, parameters: csbParams },
+      }).json(),
+    [csbParams],
+    { skip: !csbParams },
+  );
+
+  return (
+    <ThemeProvider>
+      {!!csbParams && (
+        <Box position="fixed" top="8px" right="8px">
+          {sandbox.status === 'fulfilled' ? (
+            <Fab
+              size="small"
+              color="primary"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={`https://codesandbox.io/s/${sandbox.value.sandbox_id}?module=demo.tsx`}
+            >
+              <Edit fontSize="small" />
+            </Fab>
+          ) : (
+            <Fab size="small" color="primary">
+              <CircularProgress size={24} color="inherit" />{' '}
+            </Fab>
+          )}
+        </Box>
+      )}
+
+      <Box padding={2} marginTop={5}>
+        {children}
+      </Box>
+    </ThemeProvider>
+  );
+}
 
 addDecorator((Story, ctx) => (
-  <ThemeProvider>
-    <Box padding={2}>{<Story {...ctx} />}</Box>
-  </ThemeProvider>
+  <ThemeDecorator {...ctx}>
+    <Story {...ctx} />
+  </ThemeDecorator>
 ));
