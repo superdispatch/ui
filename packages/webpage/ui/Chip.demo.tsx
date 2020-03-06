89a3517f183447a6ae3d1073fbac3ab8dbda4b8b
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-03-06 16:45:38 +0500

    docs: Setup `knobs` for `Data Display` component demos.

--- packages/webpage/ui/Chip.demo.tsx
+++ packages/webpage/ui/Chip.demo.tsx
@@ -1,74 +1,45 @@
-import {
-  Card,
-  CardContent,
-  Chip,
-  FormControl,
-  FormControlLabel,
-  FormGroup,
-  FormLabel,
-  Grid,
-  Switch,
-} from '@material-ui/core';
+import { Card, CardContent, Chip, Grid } from '@material-ui/core';
 import { Call } from '@material-ui/icons';
-import { GridStack, useSnackbarStack } from '@superdispatch/ui';
-import React, { useState } from 'react';
+import { boolean } from '@storybook/addon-knobs';
+import { useSnackbarStack } from '@superdispatch/ui';
+import React from 'react';
 
 export default function ChipDemo() {
-  const [disabled, setDisabled] = useState(false);
-  const [hasIcon, setHasIcon] = useState(false);
+  const disabled = boolean('Disabled', false);
+  const hasIcon = boolean('With Icon', false);
   const { addSnackbar } = useSnackbarStack();
   const icon = hasIcon ? <Call /> : undefined;
 
   return (
-    <GridStack spacing={2}>
-      <FormControl>
-        <FormLabel>State</FormLabel>
-        <FormGroup row={true}>
-          <FormControlLabel
-            label="Disabled"
-            control={<Switch />}
-            checked={disabled}
-            onChange={(_, checked) => setDisabled(checked)}
-          />
-          <FormControlLabel
-            label="With Icon"
-            control={<Switch />}
-            checked={hasIcon}
-            onChange={(_, checked) => setHasIcon(checked)}
-          />
-        </FormGroup>
-      </FormControl>
-
-      <Card>
-        <CardContent>
-          <Grid container={true} spacing={2}>
-            <Grid item={true}>
-              <Chip label="Basic" icon={icon} disabled={disabled} />
-            </Grid>
-
-            <Grid item={true}>
-              <Chip
-                label="Clickable"
-                clickable={true}
-                icon={icon}
-                disabled={disabled}
-                onClick={() => addSnackbar('Clicked', { variant: 'success' })}
-              />
-            </Grid>
-
-            <Grid item={true}>
-              <Chip
-                label="Delete"
-                icon={icon}
-                disabled={disabled}
-                onDelete={() => {
-                  addSnackbar('Delete clicked', { variant: 'error' });
-                }}
-              />
-            </Grid>
+    <Card>
+      <CardContent>
+        <Grid container={true} spacing={2}>
+          <Grid item={true}>
+            <Chip label="Basic" icon={icon} disabled={disabled} />
           </Grid>
-        </CardContent>
-      </Card>
-    </GridStack>
+
+          <Grid item={true}>
+            <Chip
+              label="Clickable"
+              clickable={true}
+              icon={icon}
+              disabled={disabled}
+              onClick={() => addSnackbar('Clicked', { variant: 'success' })}
+            />
+          </Grid>
+
+          <Grid item={true}>
+            <Chip
+              label="Delete"
+              icon={icon}
+              disabled={disabled}
+              onDelete={() => {
+                addSnackbar('Delete clicked', { variant: 'error' });
+              }}
+            />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 }
--- packages/webpage/ui/SvgIcon.demo.tsx
+++ packages/webpage/ui/SvgIcon.demo.tsx
@@ -1,15 +1,7 @@
-import {
-  FormControl,
-  FormControlLabel,
-  FormLabel,
-  Radio,
-  RadioGroup,
-} from '@material-ui/core';
 import { SvgIconProps } from '@material-ui/core/SvgIcon';
 import { Check } from '@material-ui/icons';
 import { GridStack, InlineGrid } from '@superdispatch/ui';
-import { startCase } from 'lodash';
-import React, { useState } from 'react';
+import React from 'react';
 
 const colors: Array<SvgIconProps['color']> = [
   'inherit',
@@ -25,34 +17,15 @@ const fontSizes: Array<SvgIconProps['fontSize']> = [
 ];
 
 export default function SvgIconDemo() {
-  const [color, setColor] = useState<SvgIconProps['color']>('primary');
-
   return (
     <GridStack spacing={2}>
-      <FormControl>
-        <FormLabel>Color</FormLabel>
-        <RadioGroup
-          row={true}
-          name="color"
-          value={color}
-          onChange={(_, value) => setColor(value as SvgIconProps['color'])}
-        >
-          {colors.map(x => (
-            <FormControlLabel
-              key={x}
-              value={x}
-              control={<Radio />}
-              label={startCase(x)}
-            />
+      {colors.map(color => (
+        <InlineGrid key={color} spacing={2}>
+          {fontSizes.map(fontSize => (
+            <Check key={fontSize} color={color} fontSize={fontSize} />
           ))}
-        </RadioGroup>
-      </FormControl>
-
-      <InlineGrid spacing={2}>
-        {fontSizes.map(fontSize => (
-          <Check key={fontSize} color={color} fontSize={fontSize} />
-        ))}
-      </InlineGrid>
+        </InlineGrid>
+      ))}
     </GridStack>
   );
 }
