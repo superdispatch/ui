26b6c4cc5edffbf7b0bac6e393c7b153bf76e9ba
Umidbek Karimov <uma.karimov@gmail.com>
2020-03-09 12:25:02 +0500

    docs: Setup `knobs` for `Input` component demos.

--- packages/webpage/ui/TextField.demo.tsx
+++ packages/webpage/ui/TextField.demo.tsx
@@ -1,14 +1,6 @@
-import {
-  FormControl,
-  FormControlLabel,
-  FormGroup,
-  FormLabel,
-  InputAdornment,
-  MenuItem,
-  Switch,
-  TextField,
-} from '@material-ui/core';
+import { InputAdornment, MenuItem, TextField } from '@material-ui/core';
 import { AccountCircle } from '@material-ui/icons';
+import { boolean } from '@storybook/addon-knobs';
 import { GridStack } from '@superdispatch/ui';
 import { startCase } from 'lodash';
 import React, { useState } from 'react';
@@ -16,81 +8,20 @@ import React, { useState } from 'react';
 const subscriptionTypes = ['starter', 'pro', 'enterprise'];
 
 export default function TextFieldDemo() {
+  const disabled = boolean('Disabled', false);
+  const hasLabel = boolean('Has Label', false);
+  const hasError = boolean('Has Error', false);
+  const isFullWidth = boolean('Full Width', false);
+  const isMultiline = boolean('Multiline', false);
+  const hasHelperText = boolean('Has Helper Text', false);
+  const hasStartIcon = boolean('Has Start Icon', false);
+  const hasEndIcon = boolean('Has End Icon', false);
+
   const [text, setText] = useState('');
   const [subscriptionType, setSubscriptionType] = useState('');
-  const [disabled, setDisabled] = useState(false);
-  const [hasLabel, setHasLabel] = useState(false);
-  const [hasError, setHasError] = useState(false);
-  const [isFullWidth, setIsFullWidth] = useState(false);
-  const [isMultiline, setIsMultiline] = useState(false);
-  const [hasHelperText, setHasHelperText] = useState(false);
-  const [hasStartIcon, setHasStartIcon] = useState(false);
-  const [hasEndIcon, setHasEndIcon] = useState(false);
 
   return (
     <GridStack spacing={2}>
-      <FormControl component="fieldset">
-        <FormLabel component="legend">Visual</FormLabel>
-
-        <FormGroup row={true}>
-          <FormControlLabel
-            label="Disabled"
-            control={<Switch />}
-            checked={disabled}
-            onChange={(_, checked) => setDisabled(checked)}
-          />
-
-          <FormControlLabel
-            label="Full width"
-            control={<Switch />}
-            checked={isFullWidth}
-            onChange={(_, checked) => setIsFullWidth(checked)}
-          />
-
-          <FormControlLabel
-            label="Multiline"
-            control={<Switch />}
-            checked={isMultiline}
-            onChange={(_, checked) => setIsMultiline(checked)}
-          />
-
-          <FormControlLabel
-            label="Has Label"
-            control={<Switch />}
-            checked={hasLabel}
-            onChange={(_, checked) => setHasLabel(checked)}
-          />
-
-          <FormControlLabel
-            label="Has Error"
-            control={<Switch />}
-            checked={hasError}
-            onChange={(_, checked) => setHasError(checked)}
-          />
-
-          <FormControlLabel
-            label="Has Helper Text"
-            control={<Switch />}
-            checked={hasHelperText}
-            onChange={(_, checked) => setHasHelperText(checked)}
-          />
-
-          <FormControlLabel
-            label="Has Start Icon"
-            control={<Switch />}
-            checked={hasStartIcon}
-            onChange={(_, checked) => setHasStartIcon(checked)}
-          />
-
-          <FormControlLabel
-            label="Has End Icon"
-            control={<Switch />}
-            checked={hasEndIcon}
-            onChange={(_, checked) => setHasEndIcon(checked)}
-          />
-        </FormGroup>
-      </FormControl>
-
       <TextField
         id="email"
         value={text}
