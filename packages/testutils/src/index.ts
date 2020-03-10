7a73f8f6171e31f5c5e8520468d0b426af9a4882
Umidbek Karimov <uma.karimov@gmail.com>
2020-03-10 09:44:51 +0500

    WIP

--- packages/testutils/src/index.ts
+++ packages/testutils/src/index.ts
@@ -4,3 +4,4 @@ export * from './serializers/location-serializer';
 export * from './mockEndpoint';
 export * from './mockEndpoints';
 export * from './MockEvent';
+export * from './setupMockEndpoint';
