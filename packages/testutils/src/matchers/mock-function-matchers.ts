bfc8772401721a67c591d55d66ccccc339561298
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-03-11 17:02:23 +0500

    fix(testutils): Run all side effects inside `setupTestUtils`.

---
+++ packages/testutils/src/matchers/mock-function-matchers.ts
@@ -0,0 +1,47 @@
+declare global {
+  namespace jest {
+    interface Matchers<R, T = {}> {
+      toHaveBeenLastCalledWithMatchingInlineSnapshot<
+        U extends { [P in keyof T]: any }
+      >(
+        propertyMatchers: Partial<U>,
+        snapshot?: string,
+      ): R;
+      toHaveBeenLastCalledWithMatchingInlineSnapshot(snapshot?: string): R;
+
+      toHaveLastReturnedWithMatchingInlineSnapshot<
+        U extends { [P in keyof T]: any }
+      >(
+        propertyMatchers: Partial<U>,
+        snapshot?: string,
+      ): R;
+      toHaveLastReturnedWithMatchingInlineSnapshot(snapshot?: string): R;
+    }
+  }
+}
+
+export async function setupMockFunctionMatchers() {
+  const { toMatchInlineSnapshot } = await import('jest-snapshot');
+
+  expect.extend({
+    toHaveBeenLastCalledWithMatchingInlineSnapshot(
+      received: unknown,
+      ...args: any[]
+    ) {
+      const calls = (received as jest.Mock)?.mock?.calls;
+      const lastCall = calls?.[calls?.length - 1];
+
+      return toMatchInlineSnapshot.call(this as any, lastCall, ...args);
+    },
+
+    toHaveLastReturnedWithMatchingInlineSnapshot(
+      received: unknown,
+      ...args: any[]
+    ) {
+      const results = (received as jest.Mock)?.mock?.results;
+      const lastResult = results?.[results?.length - 1]?.value;
+
+      return toMatchInlineSnapshot.call(this as any, lastResult, ...args);
+    },
+  });
+}
