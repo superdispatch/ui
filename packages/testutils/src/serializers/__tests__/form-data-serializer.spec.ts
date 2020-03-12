7514505f0a8dc863f0fae7f67b50359eabd7e4bc
doniyor2109 <doniyor2109@gmail.com>
2020-03-12 14:47:56 +0500

    feat(testutils): Add Serializer for FormData (#64)

---
+++ packages/testutils/src/serializers/__tests__/form-data-serializer.spec.ts
@@ -0,0 +1,35 @@
+import { setupFormDataSerializer } from '../form-data-serializer';
+
+setupFormDataSerializer();
+
+it('serializes empty FormData', () => {
+  expect(new FormData()).toMatchInlineSnapshot(`FormData {}`);
+});
+
+it('serializes FormData with some data', () => {
+  const formData = new FormData();
+
+  formData.append('name', 'John');
+  formData.append('file', new Blob([], { type: 'image/jpg' }));
+
+  expect(formData).toMatchInlineSnapshot(`
+    FormData {
+      "file": File {},
+      "name": "John",
+    }
+  `);
+});
+
+it('serialize composed FormData', () => {
+  const formData = new FormData();
+  formData.append('name', 'John');
+
+  expect({ foo: 'bar', form: formData }).toMatchInlineSnapshot(`
+    Object {
+      "foo": "bar",
+      "form": FormData {
+        "name": "John",
+      },
+    }
+  `);
+});
--- packages/testutils/src/setupTestUtils.ts
+++ packages/testutils/src/setupTestUtils.ts
@@ -2,11 +2,13 @@ import { act } from '@testing-library/react';
 import fetchMock from 'fetch-mock';
 
 import { setupMockFunctionMatchers } from './matchers/mock-function-matchers';
+import { setupFormDataSerializer } from './serializers/form-data-serializer';
 import { setupLocationSerializer } from './serializers/location-serializer';
 
 export * from './matchers/mock-function-matchers';
 
 export function setupTestUtils() {
+  setupFormDataSerializer();
   setupLocationSerializer();
   setupMockFunctionMatchers();
 
