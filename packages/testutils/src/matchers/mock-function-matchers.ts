54bbaf8c3af50b7de0f3d1a8d8d27a9be3655f62
Umidbek Karimov <uma.karimov@gmail.com>
2020-03-12 17:26:36 +0500

    feat(testutils): Remove `propertyMatchers` from `toHaveLastReturnedWithMatchingInlineSnapshot`.

--- packages/testutils/src/matchers/mock-function-matchers.ts
+++ packages/testutils/src/matchers/mock-function-matchers.ts
@@ -3,18 +3,12 @@ import { toMatchInlineSnapshot } from 'jest-snapshot';
 declare global {
   namespace jest {
     interface Matchers<R, T = {}> {
-      toHaveBeenLastCalledWithMatchingInlineSnapshot<
-        U extends { [P in keyof T]: any }
-      >(
-        propertyMatchers: Partial<U>,
-        snapshot?: string,
-      ): R;
       toHaveBeenLastCalledWithMatchingInlineSnapshot(snapshot?: string): R;
 
-      toHaveLastReturnedWithMatchingInlineSnapshot<
-        U extends { [P in keyof T]: any }
-      >(
-        propertyMatchers: Partial<U>,
+      toHaveLastReturnedWithMatchingInlineSnapshot(
+        propertyMatchers: Partial<
+          T extends (...args: any[]) => any ? ReturnType<T> : object
+        >,
         snapshot?: string,
       ): R;
       toHaveLastReturnedWithMatchingInlineSnapshot(snapshot?: string): R;
@@ -24,20 +18,14 @@ declare global {
 
 export function setupMockFunctionMatchers() {
   expect.extend({
-    toHaveBeenLastCalledWithMatchingInlineSnapshot(
-      received: unknown,
-      ...args: any[]
-    ) {
+    toHaveBeenLastCalledWithMatchingInlineSnapshot(received: unknown, ...args) {
       const calls = (received as jest.Mock)?.mock?.calls;
       const lastCall = calls?.[calls?.length - 1];
 
       return toMatchInlineSnapshot.call(this as any, lastCall, ...args);
     },
 
-    toHaveLastReturnedWithMatchingInlineSnapshot(
-      received: unknown,
-      ...args: any[]
-    ) {
+    toHaveLastReturnedWithMatchingInlineSnapshot(received: unknown, ...args) {
       const results = (received as jest.Mock)?.mock?.results;
       const lastResult = results?.[results?.length - 1]?.value;
 
