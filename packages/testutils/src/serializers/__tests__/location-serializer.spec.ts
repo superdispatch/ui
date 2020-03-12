122e044177cd05e33d6d98e3a242d9607bcd66a8
Umidbek Karimov <uma.karimov@gmail.com>
2020-03-12 16:49:26 +0500

    feat(testutils): Do not print empty properties in location serializer.

---
+++ packages/testutils/src/serializers/__tests__/location-serializer.spec.ts
@@ -0,0 +1,66 @@
+import { parse } from 'url';
+
+import { setupLocationSerializer } from '../location-serializer';
+
+setupLocationSerializer();
+
+function makeLocation(url: string) {
+  const { pathname, search, hash } = parse(url);
+
+  return { pathname, search, hash };
+}
+
+it('serializes location pathname', () => {
+  expect(makeLocation('/hello')).toMatchInlineSnapshot(`
+Object {
+  "pathname": "/hello",
+}
+`);
+});
+
+it('serializes location hash', () => {
+  expect(makeLocation('/hello#there')).toMatchInlineSnapshot(`
+Object {
+  "hash": "#there",
+  "pathname": "/hello",
+}
+`);
+});
+
+it('serializes location search', () => {
+  expect(makeLocation('/hello?foo=bar&baz=3')).toMatchInlineSnapshot(`
+Object {
+  "pathname": "/hello",
+  "searchParams": Object {
+    "baz": 3,
+    "foo": "bar",
+  },
+}
+`);
+});
+
+it('serializes json values inside location search', () => {
+  expect(
+    makeLocation(
+      `/hello?foo=bar&baz=${JSON.stringify({
+        this: { is: { nested: { value: 'foo' } } },
+      })}`,
+    ),
+  ).toMatchInlineSnapshot(`
+Object {
+  "pathname": "/hello",
+  "searchParams": Object {
+    "baz": Object {
+      "this": Object {
+        "is": Object {
+          "nested": Object {
+            "value": "foo",
+          },
+        },
+      },
+    },
+    "foo": "bar",
+  },
+}
+`);
+});
