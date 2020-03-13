7e6c2405277b22a63c86f4d5d2f6fb7365226ce1
Umidbek Karimov <uma.karimov@gmail.com>
2020-03-13 13:57:41 +0500

    WIP

---
+++ packages/testutils/src/__tests__/mockEndpoints.spec.ts
@@ -0,0 +1,59 @@
+import { mockEndpoints } from '../mockEndpoints';
+import { setupTestUtils } from '../setupTestUtils';
+
+setupTestUtils();
+
+it('creates dictionary of endpoints', async () => {
+  const endpoints = mockEndpoints({
+    foo: { matcher: '/', method: 'GET', response: {} },
+    bar: { matcher: '/', method: 'POST', response: {} },
+  });
+
+  expect(endpoints.foo).toHaveBeenCalledTimes(0);
+
+  await fetch('http://host/');
+
+  expect(endpoints.foo).toHaveBeenCalledTimes(1);
+  expect(endpoints.foo).toHaveBeenLastCalledWith({ pathname: '/' });
+
+  expect(endpoints.bar).toHaveBeenCalledTimes(0);
+
+  await fetch('http://host/', { method: 'POST' });
+
+  expect(endpoints.bar).toHaveBeenCalledTimes(1);
+  expect(endpoints.bar).toHaveBeenLastCalledWith({ pathname: '/' });
+});
+
+it('allows to pass default headers for all endpoints', async () => {
+  const endpoints = mockEndpoints(
+    {
+      foo: { matcher: '/', method: 'GET', response: {} },
+      bar: { matcher: '/', method: 'POST', response: {} },
+    },
+    {
+      headers: { Foo: 'Bar' },
+    },
+  );
+
+  expect(endpoints.foo).toHaveBeenCalledTimes(0);
+
+  await fetch('http://host/');
+
+  expect(endpoints.foo).toHaveBeenCalledTimes(0);
+
+  await fetch('http://host/', { headers: { Foo: 'Bar' } });
+
+  expect(endpoints.foo).toHaveBeenCalledTimes(1);
+  expect(endpoints.foo).toHaveBeenLastCalledWith({ pathname: '/' });
+
+  expect(endpoints.bar).toHaveBeenCalledTimes(0);
+
+  await fetch('http://host/', { method: 'POST' });
+
+  expect(endpoints.bar).toHaveBeenCalledTimes(0);
+
+  await fetch('http://host/', { method: 'POST', headers: { Foo: 'Bar' } });
+
+  expect(endpoints.bar).toHaveBeenCalledTimes(1);
+  expect(endpoints.bar).toHaveBeenLastCalledWith({ pathname: '/' });
+});
