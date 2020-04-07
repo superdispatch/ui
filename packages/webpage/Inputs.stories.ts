b167a86cb723a70b158792b0e9c43a9861856ee4
doniyor2109 <doniyor2109@gmail.com>
2020-04-07 19:45:16 +0500

    WIP

--- packages/webpage/Inputs.stories.ts
+++ packages/webpage/Inputs.stories.ts
@@ -8,3 +8,4 @@ export { default as IconButton } from './ui/IconButton.demo';
 export { default as Radio } from './ui/Radio.demo';
 export { default as Switch } from './ui/Switch.demo';
 export { default as TextField } from './ui/TextField.demo';
+export { default as Forms } from './ui/Forms.demo';
