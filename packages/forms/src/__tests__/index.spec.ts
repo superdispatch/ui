f98eec7283bcaf366ae0132f1ca9e7216649c80d
doniyor2109 <doniyor2109@gmail.com>
2020-04-07 11:25:41 +0500

    WIP

--- packages/forms/src/__tests__/index.spec.ts
+++ packages/forms/src/__tests__/index.spec.ts
@@ -4,9 +4,7 @@ it('exposes public api', () => {
   expect(api).toMatchInlineSnapshot(`
     Object {
       "FormikCheckboxField": [Function],
-      "FormikCurrencyField": [Function],
       "FormikDateField": [Function],
-      "FormikNumberField": [Function],
       "FormikPhoneField": [Function],
       "FormikRadioGroupField": [Function],
       "FormikTextField": [Function],
