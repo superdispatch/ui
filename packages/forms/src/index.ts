609c9f2ac4d5eec98a159e73bc73174485bfaf37
doniyor2109 <doniyor2109@gmail.com>
2020-04-07 11:25:11 +0500

    WIP

--- packages/forms/src/index.ts
+++ packages/forms/src/index.ts
@@ -1,9 +1,7 @@
 export * from './useAppFormik';
 
 export * from './FormikTextField';
-export * from './FormikNumberField';
 export * from './FormikCheckboxField';
 export * from './FormikRadioGroupField';
 export * from './FormikDateField';
-export * from './FormikCurrencyField';
 export * from './FormikPhoneField';
