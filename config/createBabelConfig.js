4c0adb59f835aab390387fbde330b3a6181cd0d6
Umidbek Karimov <uma.karimov@gmail.com>
2020-04-14 03:59:25 +0500

    chore: Disable `babel-plugin-direct-import` for build.

--- config/createBabelConfig.js
+++ config/createBabelConfig.js
@@ -11,17 +11,6 @@ module.exports = () => (api) => {
       'babel-plugin-annotate-pure-calls',
       require.resolve('../tools/babel-plugin-inject-display-name'),
       ['babel-plugin-optimize-clsx', { libraries: ['clsx'] }],
-      !isTest && [
-        'babel-plugin-direct-import',
-        {
-          modules: [
-            '@material-ui/lab',
-            '@material-ui/core',
-            '@material-ui/icons',
-            '@material-ui/styles',
-          ],
-        },
-      ],
-    ].filter(Boolean),
+    ],
   };
 };
