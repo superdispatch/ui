2e2c4e5a48dee1d7f18b8684c057c617b8404f5e
doniyor2109 <doniyor2109@gmail.com>
2020-04-14 18:14:31 +0500

    feat(testutils): Add blob serializer.

--- packages/testutils/src/serializers/__tests__/form-data-serializer.spec.ts
+++ packages/testutils/src/serializers/__tests__/form-data-serializer.spec.ts
@@ -10,9 +10,15 @@ it('serializes FormData with some data', () => {
   const formData = new FormData();
 
   formData.append('name', 'John');
+  formData.append('file', new File([], 'profile.jpg', { type: 'image/jpg' }));
 
   expect(formData).toMatchInlineSnapshot(`
     FormData {
+      "file": File {
+        "name": "profile.jpg",
+        "size": 0,
+        "type": "image/jpg",
+      },
       "name": "John",
     }
   `);
@@ -23,11 +29,11 @@ it('serialize composed FormData', () => {
   formData.append('name', 'John');
 
   expect({ foo: 'bar', form: formData }).toMatchInlineSnapshot(`
-Object {
-  "foo": "bar",
-  "form": FormData {
-    "name": "John",
-  },
-}
-`);
+    Object {
+      "foo": "bar",
+      "form": FormData {
+        "name": "John",
+      },
+    }
+  `);
 });
