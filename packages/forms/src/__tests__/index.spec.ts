d411ca72ef2fd37b3cc59d81da21d9187b453a1f
doniyor2109 <doniyor2109@gmail.com>
2020-04-16 21:41:59 +0500

    feat(forms): Rename AppFormik to FormikEnhanced. (#80)

--- packages/forms/src/__tests__/index.spec.ts
+++ packages/forms/src/__tests__/index.spec.ts
@@ -3,14 +3,14 @@ import * as api from '..';
 it('exposes public api', () => {
   expect(api).toMatchInlineSnapshot(`
     Object {
-      "AppFormikProvider": [Function],
       "FormikCheckboxField": [Function],
       "FormikDateField": [Function],
       "FormikPhoneField": [Function],
       "FormikRadioGroupField": [Function],
       "FormikTextField": [Function],
-      "useAppFormik": [Function],
-      "useAppFormikContext": [Function],
+      "FormsProvider": [Function],
+      "useFormikEnhanced": [Function],
+      "useFormsContext": [Function],
       "validatePhone": [Function],
     }
   `);
