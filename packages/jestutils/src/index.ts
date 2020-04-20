28f7d3e4fde9ef17707ac3355b6370361599c95b
Umidbek Karimov <uma.karimov@gmail.com>
2020-04-20 11:05:39 +0500

    feat(jestutils): Add `toBeSameDate` matcher.

--- packages/jestutils/src/index.ts
+++ packages/jestutils/src/index.ts
@@ -2,8 +2,10 @@ import './serializers/blob-serializer';
 import './serializers/form-data-serializer';
 import './serializers/location-serializer';
 
+export * from './matchers/date-matchers';
 export * from './matchers/mock-function-matchers';
 
+export * from './spyLogs';
 export * from './mockEndpoint';
 export * from './mockEndpoints';
 export * from './MockEvent';
