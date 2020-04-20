f1cce6c369a154c10af0dbda7a7b11cca25558a1
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-04-20 11:05:39 +0500

    feat(jestutils): Add `toBeSameDate` matcher.

--- setupTests.ts
+++ setupTests.ts
@@ -1,55 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
 
-import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils';
+import { spyLogs } from '@superdispatch/jestutils';
 
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace jest {
-    interface Matchers<R> {
-      toBeSameDate(expected: unknown): R;
-    }
-  }
-}
-
-beforeEach(() => {
-  jest.spyOn(console, 'warn');
-  jest.spyOn(console, 'error');
-});
-
-afterEach(() => {
-  // eslint-disable-next-line no-console
-  expect(console.warn).not.toBeCalled();
-  // eslint-disable-next-line no-console
-  expect(console.error).not.toBeCalled();
-});
-
-expect.extend({
-  toBeSameDate(received: unknown, expected: unknown) {
-    const pass =
-      (received == null && expected == null) ||
-      (received instanceof Date &&
-        expected instanceof Date &&
-        (Object.is(received, expected) ||
-          Object.is(received.getTime(), expected.getTime())));
-
-    return {
-      pass,
-      message: () =>
-        !pass
-          ? [
-              matcherHint('.toBeSameDate'),
-              '',
-              'Expected values to be same:',
-              `Expected: ${printExpected(expected)}`,
-              `Received: ${printReceived(received)}`,
-            ].join('\n')
-          : [
-              matcherHint('.not.toBeSameDate'),
-              '',
-              'Expected values to not be same:',
-              `Expected: ${printExpected(expected)}`,
-              `Received: ${printReceived(received)}`,
-            ].join('\n'),
-    };
-  },
-});
+spyLogs({ warn: 'forbid', error: 'forbid' });
