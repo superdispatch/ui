b8600e513623567cb75f0fd60e6f32d4c46af5b4
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-06-09 15:00:01 +0500

    fix(webpage): Add Edit in CodeSandbox button back.

--- packages/webpage/.storybook/preview.tsx
+++ packages/webpage/.storybook/preview.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from '@superdispatch/ui';
 import { Box, Fab } from '@material-ui/core';
 import { StoryContext } from '@storybook/addons';
 import { get } from 'lodash';
-import { usePromise } from 'utility-hooks';
 import { Edit } from '@material-ui/icons';
 import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
@@ -20,36 +19,12 @@ addDecorator((Story, ctx) => (
 
 function ThemeDecorator({ children, ...ctx }: PropsWithChildren<StoryContext>) {
   const Component = ctx.hooks?.mountedDecorators?.values().next().value;
-  const pr = get(Component, 'pr');
   const branch = get(Component, 'branch');
   const filename = get(Component, 'filename');
-  const prCSB = usePromise(
-    async ({ abortSignal }) => {
-      const response = await fetch(
-        `https://gh.csb.dev/api/superdispatch/ui/prs/${pr}/builds`,
-        { signal: abortSignal },
-      );
-      const data: {
-        builds: Array<{
-          status?: string;
-          sandboxes?: Array<{ url: string }>;
-        }>;
-      } = await response.json();
-      const lastBuild = data.builds.find(
-        (build) => build.status === 'succeeded',
-      );
 
-      return lastBuild?.sandboxes?.[0].url;
-    },
-    [],
-    { skip: !pr },
-  );
-
-  const branchCSB = !branch
+  const csbURL = !branch
     ? undefined
     : `https://codesandbox.io/s/github/superdispatch/ui/tree/${branch}/packages/webpage`;
-
-  const csbURL = pr ? prCSB.value : branchCSB;
 
   const csbParams = new URLSearchParams({
     module: filename,
