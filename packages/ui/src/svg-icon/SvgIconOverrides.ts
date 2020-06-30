caee47fa7b7eeffcdaf30ce94351f7e280427218
Umidbek Karimov <uma.karimov@gmail.com>
2020-06-30 12:29:32 +0500

    feat(ui): [SvgIcon] inherit display style from parent.

--- packages/ui/src/svg-icon/SvgIconOverrides.ts
+++ packages/ui/src/svg-icon/SvgIconOverrides.ts
@@ -5,15 +5,23 @@ export function overrideSvgIcon(theme: SuperDispatchTheme) {
   const sm = theme.breakpoints.up('sm');
 
   theme.overrides.MuiSvgIcon = {
-    colorAction: { color: Color.Grey100 },
     root: {
+      display: 'inherit',
       fontSize: theme.spacing(4),
       [sm]: { fontSize: theme.spacing(3) },
     },
+
     fontSizeSmall: {
       fontSize: theme.spacing(3),
       [sm]: { fontSize: theme.spacing(2) },
     },
-    fontSizeLarge: { fontSize: theme.spacing(4) },
+
+    fontSizeLarge: {
+      fontSize: theme.spacing(4),
+    },
+
+    colorAction: {
+      color: Color.Grey100,
+    },
   };
 }
--- packages/ui/src/svg-icon/__tests__/SvgIcon.spec.tsx
+++ packages/ui/src/svg-icon/__tests__/SvgIcon.spec.tsx
@@ -14,7 +14,7 @@ it('checks component css', () => {
       fill: currentColor;
       width: 1em;
       height: 1em;
-      display: inline-block;
+      display: inherit;
       font-size: 32px;
       transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
       flex-shrink: 0;
