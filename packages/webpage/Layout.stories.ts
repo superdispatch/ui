0cee1536d5ef2811d3badd460834d6ee76dbe669
Umidbek Karimov <uma.karimov@gmail.com>
2020-07-22 15:29:20 +0500

    docs(ui): [Stack] make Playroom demos.

--- packages/webpage/Layout.stories.ts
+++ packages/webpage/Layout.stories.ts
@@ -2,4 +2,3 @@ export default { title: 'Layout' };
 
 export { default as Columns } from './ui/Columns.demo';
 export { default as Grids } from './ui/Grids.demo';
-export { default as Stack } from './ui/Stack.demo';
