eb6c1e4a2b4b36c658d4831fdb0c6872d19c7c79
Umidbek Karimov <uma.karimov@gmail.com>
2020-08-10 13:24:13 +0500

    chore: Bump dependencies.

--- setupTests.ts
+++ setupTests.ts
@@ -5,3 +5,12 @@ import { spyLogs } from '@superdispatch/jestutils';
 process.env.TZ = 'US/Arizona';
 
 spyLogs({ warn: 'forbid', error: 'forbid' });
+
+// Mock `getComputedStyle` to workaround `accessible-name-and-description`
+// selector of the `dom-accessibility-api`.
+Object.defineProperty(window, 'getComputedStyle', {
+  writable: false,
+  enumerable: false,
+  configurable: false,
+  value: () => ({ getPropertyValue: () => '' }),
+});
