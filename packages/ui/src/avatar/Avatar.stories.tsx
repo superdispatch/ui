a47d324da747b9311337d1beeac74ebeefe873b3
Umidbek Karimov <uma.karimov@gmail.com>
2020-08-13 10:35:30 +0500

    WIP

--- packages/ui/src/avatar/Avatar.stories.tsx
+++ packages/ui/src/avatar/Avatar.stories.tsx
@@ -1,14 +1,6 @@
-import { Avatar, AvatarProps } from '@material-ui/core';
-import { Meta } from '@storybook/react';
+import { Avatar } from '@material-ui/core';
 import { PropsLink } from '@superdispatch/ui-docs';
 import React from 'react';
-
-const images = [
-  'https://source.unsplash.com/256x256/?dog',
-  'https://source.unsplash.com/256x256/?cat',
-  'https://source.unsplash.com/256x256/?face',
-  'https://source.unsplash.com/256x256/?nature',
-];
 
 export default {
   title: 'Data Display/Avatar',
@@ -17,35 +9,10 @@ export default {
       <PropsLink url="https://material-ui.com/api/avatar/#props" />
     ),
   },
+};
 
-  decorators: [
-    (Story) => (
-      <>
-        <Story />
-
-        {images.map((src) => (
-          <img hidden={true} key={src} alt={src} src={src} />
-        ))}
-      </>
-    ),
-  ],
-
-  argTypes: {
-    children: {
-      table: { type: { summary: 'ReactNode' } },
-      control: { type: 'text' },
-    },
-
-    src: {
-      table: { type: { summary: 'string' } },
-      control: {
-        type: 'select',
-        options: [undefined, ...images],
-      },
-    },
-  },
-} as Meta;
-
-export const basic = (props: AvatarProps) => <Avatar {...props} />;
-
-basic.args = { children: 'A1' };
+export const basic = () => <Avatar />;
+export const text = () => <Avatar>A1</Avatar>;
+export const image = () => (
+  <Avatar src="https://source.unsplash.com/featured/?face" />
+);
