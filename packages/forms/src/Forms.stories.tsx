12e9ed9f5242729cb3d32ca67865fb465a3da6c4
Umidbek Karimov <uma.karimov@gmail.com>
2020-08-14 12:02:03 +0500

    WIP

--- packages/forms/src/Forms.stories.tsx
+++ packages/forms/src/Forms.stories.tsx
@@ -17,83 +17,75 @@ import { FormikEnhanced, FormikTextField } from '.';
 
 export default { title: 'Recipes/Forms' };
 
-export const loginForm = () => (
+export const login = () => (
   <Box maxWidth={240}>
-    <Card>
-      <CardContent>
-        <FormikEnhanced
-          initialValues={{ username: '', password: '', $showPassword: false }}
-          getFormErrors={(error) => ({ username: error.message })}
-          onSubmit={({ username, password }) =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                if (username === 'john' && password === 'doe') {
-                  alert('Successfully logged in');
-                  resolve();
-                } else {
-                  reject(new Error('Invalid username or password'));
-                }
-              }, 1000);
-            })
-          }
-          validate={({ username, password }) => {
-            if (!username) {
-              return { username: 'Username is required' };
+    <FormikEnhanced
+      initialValues={{ username: '', password: '', $showPassword: false }}
+      getFormErrors={(error) => ({ username: error.message })}
+      onSubmit={({ username, password }) =>
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (username === 'john' && password === 'doe') {
+              alert('Successfully logged in');
+              resolve();
+            } else {
+              reject(new Error('Invalid username or password'));
             }
+          }, 1000);
+        })
+      }
+      validate={({ username, password }) => {
+        if (!username) {
+          return { username: 'Username is required' };
+        }
 
-            if (!password) {
-              return { password: 'Password is required' };
-            }
+        if (!password) {
+          return { password: 'Password is required' };
+        }
 
-            return {};
-          }}
-        >
-          {({ isSubmitting, setFieldValue, values: { $showPassword } }) => (
-            <Form>
-              <Stack>
-                <FormikTextField
-                  name="username"
-                  fullWidth={true}
-                  label="Username or Email"
-                  helperText="Try `john`"
-                />
-                <FormikTextField
-                  name="password"
-                  fullWidth={true}
-                  label="Password"
-                  helperText="Try `doe`"
-                  type={$showPassword ? 'text' : 'password'}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          onClick={() => {
-                            setFieldValue('$showPassword', !$showPassword);
-                          }}
-                        >
-                          {$showPassword ? (
-                            <VisibilityOffIcon />
-                          ) : (
-                            <VisibilityIcon />
-                          )}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
+        return {};
+      }}
+    >
+      {({ isSubmitting, setFieldValue, values: { $showPassword } }) => (
+        <Form>
+          <Stack>
+            <FormikTextField
+              name="username"
+              fullWidth={true}
+              label="Username or Email"
+              helperText="Try `john`"
+            />
+            <FormikTextField
+              name="password"
+              fullWidth={true}
+              label="Password"
+              helperText="Try `doe`"
+              type={$showPassword ? 'text' : 'password'}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() => {
+                        setFieldValue('$showPassword', !$showPassword);
+                      }}
+                    >
+                      {$showPassword ? (
+                        <VisibilityOffIcon />
+                      ) : (
+                        <VisibilityIcon />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
 
-                <Button
-                  type="submit"
-                  variant="contained"
-                  isLoading={isSubmitting}
-                >
-                  Login
-                </Button>
-              </Stack>
-            </Form>
-          )}
-        </FormikEnhanced>
-      </CardContent>
-    </Card>
+            <Button type="submit" variant="contained" isLoading={isSubmitting}>
+              Login
+            </Button>
+          </Stack>
+        </Form>
+      )}
+    </FormikEnhanced>
   </Box>
 );
