6e1c9e2dec93034d5f3d99a36944f36372c9ff2a
Umidbek Karimov <uma.karimov@gmail.com>
2020-08-14 11:24:32 +0500

    WIP

---
+++ packages/ui/src/radio/RadioField.stories.tsx
@@ -0,0 +1,44 @@
+import {
+  FormControlLabel,
+  FormGroup,
+  Switch,
+  TextField,
+} from '@material-ui/core';
+import React from 'react';
+
+import { CheckboxField, RadioField, RadioGroupField } from '..';
+
+export default { title: 'Inputs/RadioField', component: RadioField };
+
+export const basic = () => (
+  <RadioField label="Label" helperText="Helper Text" />
+);
+
+export const error = () => (
+  <RadioField label="Label" helperText="Error Text" error={true} />
+);
+
+export const disabled = () => (
+  <RadioGroupField label="Disabled">
+    <RadioField label="Checked" checked={true} disabled={true} />
+    <RadioField label="Unchecked" checked={false} disabled={true} />
+  </RadioGroupField>
+);
+
+export const labelPlacement = () => (
+  <RadioField
+    label="Label"
+    FormControlLabelProps={{ labelPlacement: 'start' }}
+  />
+);
+
+export const inlineForm = () => (
+  <RadioGroupField RadioGroupProps={{ row: true }}>
+    <FormGroup row={true}>
+      <RadioField label="Radio" />
+      <CheckboxField label="Checkbox" />
+      <FormControlLabel label="Switch" control={<Switch />} />
+      <TextField placeholder="Text Field" />
+    </FormGroup>
+  </RadioGroupField>
+);
--- packages/webpage/Inputs.stories.ts
+++ packages/webpage/Inputs.stories.ts
@@ -1,8 +1,4 @@
 export default { title: 'Inputs' };
 
-export { default as Checkbox } from './ui/Checkbox.demo';
-export { default as IconButton } from './ui/IconButton.demo';
-export { default as Radio } from './ui/Radio.demo';
-export { default as Switch } from './ui/Switch.demo';
 export { default as TextField } from './ui/TextField.demo';
 export { default as Forms } from './ui/Forms.demo';
