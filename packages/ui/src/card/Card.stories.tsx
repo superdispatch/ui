c0abf64df4e42a4d4ddc0a87baea846301d5fb84
Umidbek Karimov <uma.karimov@gmail.com>
2020-08-17 12:02:15 +0500

    docs(ui): [Card] setup `@storybook/addon-docs`.

---
+++ packages/ui/src/card/Card.stories.tsx
@@ -0,0 +1,39 @@
+import { Box, Card, CardContent, Typography } from '@material-ui/core';
+import { Meta } from '@storybook/react';
+import { PropsLink } from '@superdispatch/ui-docs';
+import React from 'react';
+
+export default {
+  title: 'Surfaces/Card',
+  parameters: {
+    componentSubtitle: (
+      <PropsLink url="https://material-ui.com/api/card/#props" />
+    ),
+  },
+} as Meta;
+
+export const basic = () => (
+  <Box maxWidth={280}>
+    <Card>
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom={true}>
+          Word of the Day
+        </Typography>
+
+        <Typography variant="h5" component="h2" gutterBottom={true}>
+          be•nev•o•lent
+        </Typography>
+
+        <Typography color="textSecondary" gutterBottom={true}>
+          adjective
+        </Typography>
+
+        <Typography component="p" gutterBottom={true}>
+          well meaning and kindly.
+          <br />
+          ”a benevolent smile”
+        </Typography>
+      </CardContent>
+    </Card>
+  </Box>
+);
