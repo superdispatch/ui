0915000648eb7249a754329b8ee1e43ba3671326
Umidbek Karimov <uma.karimov@gmail.com>
2020-08-17 11:13:31 +0500

    docs(ui): [Drawer] setup `@storybook/addon-docs`.

--- packages/webpage/Navigation.stories.ts
+++ packages/webpage/Navigation.stories.ts
@@ -1,6 +1,5 @@
 export default { title: 'Navigation' };
 
-export { default as Drawer } from './ui/Drawer.demo';
 export { default as Link } from './ui/Link.demo';
 export { default as Menu } from './ui/Menu.demo';
 export { default as Tabs } from './ui/Tabs.demo';
