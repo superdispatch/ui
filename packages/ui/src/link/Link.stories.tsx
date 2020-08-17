268c73b371aeacb7fe6e3a01c29a3e773952f53e
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-08-17 11:19:33 +0500

    docs(ui): [Link] setup `@storybook/addon-docs`.

---
+++ packages/ui/src/link/Link.stories.tsx
@@ -0,0 +1,42 @@
+import { Box, Link, Typography } from '@material-ui/core';
+import { Meta } from '@storybook/react';
+import React from 'react';
+
+import { Stack } from '..';
+
+export default { title: 'Navigation/Link' } as Meta;
+
+export const basic = () => (
+  <Typography>
+    This is a{' '}
+    <Link href="#" target="_blank">
+      link
+    </Link>{' '}
+    in text with a <Link component="button">button link</Link>.
+  </Typography>
+);
+
+export const multiline = () => (
+  <Box maxWidth={64}>
+    <Link href="#" target="_blank">
+      This is multi-line link
+    </Link>
+  </Box>
+);
+
+export const multilineButton = () => (
+  <Box maxWidth={64}>
+    <Link component="button">This is multi-line button link</Link>
+  </Box>
+);
+
+export const colors = () => (
+  <Stack>
+    <Link href="#" target="_blank" color="primary">
+      Primary
+    </Link>
+    <Link href="#" target="_blank" color="textPrimary">
+      Text Primary
+    </Link>
+  </Stack>
+);
--- packages/webpage/Navigation.stories.ts
+++ packages/webpage/Navigation.stories.ts
@@ -1,5 +1,4 @@
 export default { title: 'Navigation' };
 
-export { default as Link } from './ui/Link.demo';
 export { default as Menu } from './ui/Menu.demo';
 export { default as Tabs } from './ui/Tabs.demo';
