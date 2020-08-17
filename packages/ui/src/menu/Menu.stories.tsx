3fae913114d0796f7182d163e74f5842fcc8c435
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-08-17 11:49:17 +0500

    docs(ui): [Menu] setup `@storybook/addon-docs`.

---
+++ packages/ui/src/menu/Menu.stories.tsx
@@ -0,0 +1,29 @@
+import { Button, Menu, MenuItem } from '@material-ui/core';
+import { Meta } from '@storybook/react';
+import { UseState } from '@superdispatch/ui-playroom/UseState';
+import React from 'react';
+
+export default { title: 'Navigation/Menu' } as Meta;
+
+export const basic = () => (
+  <UseState
+    initialState={null}
+    render={(state, setState) => (
+      <>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={(event) => setState(event.currentTarget)}
+        >
+          Menu
+        </Button>
+
+        <Menu open={!!state} anchorEl={state} onClose={() => setState(false)}>
+          <MenuItem>Option 1</MenuItem>
+          <MenuItem>Option 2</MenuItem>
+          <MenuItem>Option 3</MenuItem>
+        </Menu>
+      </>
+    )}
+  />
+);
--- packages/webpage/Navigation.stories.ts
+++ packages/webpage/Navigation.stories.ts
@@ -1,4 +1,3 @@
 export default { title: 'Navigation' };
 
-export { default as Menu } from './ui/Menu.demo';
 export { default as Tabs } from './ui/Tabs.demo';
