b1c33252247133831f919579e1e3f909e6e11f8e
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-08-18 13:49:57 +0500

    docs(ui): [AppBar] setup `@storybook/addon-docs`.

---
+++ packages/ui/src/app-bar/AppBar.stories.tsx
@@ -0,0 +1,64 @@
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { ArrowBack, Close } from '@material-ui/icons';
+import { Meta } from '@storybook/react';
+import { PropsLink } from '@superdispatch/ui-docs';
+import React from 'react';
+
+import { Column, Columns } from '..';
+
+export default {
+  title: 'Surfaces/AppBar',
+  parameters: {
+    componentSubtitle: (
+      <PropsLink url="https://material-ui.com/api/app-bar/#props" />
+    ),
+  },
+} as Meta;
+
+export const basic = () => (
+  <AppBar>
+    <Toolbar>
+      <Columns align="center">
+        <Column width="content">
+          <IconButton edge="start" aria-label="back">
+            <ArrowBack />
+          </IconButton>
+        </Column>
+
+        <Column>
+          <Typography variant="h2">App Bar</Typography>
+        </Column>
+
+        <Column width="content">
+          <IconButton edge="end" aria-label="close">
+            <Close />
+          </IconButton>
+        </Column>
+      </Columns>
+    </Toolbar>
+  </AppBar>
+);
+
+export const dense = () => (
+  <AppBar>
+    <Toolbar variant="dense">
+      <Columns align="center">
+        <Column width="content">
+          <IconButton edge="start" aria-label="back">
+            <ArrowBack />
+          </IconButton>
+        </Column>
+
+        <Column>
+          <Typography variant="h2">App Bar</Typography>
+        </Column>
+
+        <Column width="content">
+          <IconButton edge="end" aria-label="close">
+            <Close />
+          </IconButton>
+        </Column>
+      </Columns>
+    </Toolbar>
+  </AppBar>
+);
