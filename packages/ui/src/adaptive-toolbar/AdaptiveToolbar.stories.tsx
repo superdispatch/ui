5e8427e3380cc4d5243d64543b8043ea3e532fa9
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-08-18 13:35:23 +0500

    docs(ui): [AdaptiveToolbar] setup `@storybook/addon-docs`.

---
+++ packages/ui/src/adaptive-toolbar/AdaptiveToolbar.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta } from '@storybook/react';
+import React from 'react';
+
+import { AdaptiveToolbar } from './AdaptiveToolbar';
+
+export default {
+  title: 'Surfaces/AdaptiveToolbar',
+  component: AdaptiveToolbar,
+} as Meta;
+
+export const basic = () => (
+  <AdaptiveToolbar
+    items={[
+      'Send Offer',
+      'Post to SLB',
+      'Change Date Ranges',
+      'Increase Price By',
+      'Mark as Accepted',
+      'Send Customer Invoice',
+      'Archive',
+      'Delete',
+    ].map((label) => ({ label, key: label }))}
+  />
+);
