07024e1330a135e1da6061e35c34859fc48b6106
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-10-08 17:52:32 +0500

    test: Forbid warning and error logs only in CI.

--- setupTests.ts
+++ setupTests.ts
@@ -4,7 +4,9 @@ import { spyLogs } from '@superdispatch/jestutils';
 
 process.env.TZ = 'US/Arizona';
 
-spyLogs({ warn: 'forbid', error: 'forbid' });
+if (process.env.CI) {
+  spyLogs({ warn: 'forbid', error: 'forbid' });
+}
 
 // Mock `getComputedStyle` to workaround `accessible-name-and-description`
 // selector of the `dom-accessibility-api`.
