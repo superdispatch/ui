dd0a8e0d1bc91b9404662c9581bb0bfc0ee76e8b
Umidbek Karimov <uma.karimov@gmail.com>
2020-10-31 23:03:58 +0500

    refactor(phones): Keep country codes in the CountryCodeMetadata.

--- packages/phones/src/index.ts
+++ packages/phones/src/index.ts
@@ -1,5 +1,5 @@
 export * from './formatted-phone-number/FormattedPhoneNumber';
-export * from './data/PhoneRegionCode';
+export * from './data/CountryCodeMetadata';
 export * from './data/PhoneUtils';
 export * from './phone-field/PhoneField';
 export * from './phone-link/PhoneLink';
