93519c198b7adbf1b93a93543eff1ed164a092fa
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-11-05 13:57:55 +0500

    feat(jestutils): Remove `mock-function-matchers`.

--- packages/jestutils/src/index.ts
+++ packages/jestutils/src/index.ts
@@ -2,10 +2,8 @@ import './serializers/blob-serializer';
 import './serializers/form-data-serializer';
 import './serializers/location-serializer';
 
+export * from './MockEvent';
 export * from './matchers/date-matchers';
-export * from './matchers/mock-function-matchers';
-
-export * from './spyLogs';
 export * from './mockEndpoint';
 export * from './mockEndpoints';
-export * from './MockEvent';
+export * from './spyLogs';
