2d86ee7681d654f6e514c3db6d963074753948ff
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-11-18 17:58:17 +0500

    feat(forms): Wrap `FormikDateField` with `forwardRef`.

--- packages/forms/src/date/FormikDateField.tsx
+++ packages/forms/src/date/FormikDateField.tsx
@@ -9,55 +9,61 @@ import {
   useDateConfig,
 } from '@superdispatch/dates';
 import { useField, useFormikContext } from 'formik';
-import React, { ReactElement } from 'react';
+import React, { forwardRef, ReactElement } from 'react';
 
 export interface FormikDateFieldProps extends Omit<DateFieldProps, 'error'> {
   name: string;
   validate?: (info: DatePayload) => void | string;
 }
 
-export function FormikDateField({
-  name,
-  format,
-  onChange,
-  disabled,
-  helperText,
-  validate: validateProp,
-  ...props
-}: FormikDateFieldProps): ReactElement {
-  const config = useDateConfig({ format });
-  const { isSubmitting } = useFormikContext();
+export const FormikDateField = forwardRef<HTMLDivElement, FormikDateFieldProps>(
+  (
+    {
+      name,
+      format,
+      onChange,
+      disabled,
+      helperText,
+      validate: validateProp,
+      ...props
+    },
+    ref,
+  ): ReactElement => {
+    const config = useDateConfig({ format });
+    const { isSubmitting } = useFormikContext();
 
-  function validate(value: unknown): void | string {
-    if (!validateProp) return;
+    function validate(value: unknown): void | string {
+      if (!validateProp) return;
 
-    const dateValue = parseDate(value as NullableDateInput, config);
-    return validateProp({
-      config,
-      dateValue,
-      stringValue: stringifyDate(dateValue, config),
-    });
-  }
+      const dateValue = parseDate(value as NullableDateInput, config);
+      return validateProp({
+        config,
+        dateValue,
+        stringValue: stringifyDate(dateValue, config),
+      });
+    }
 
-  const [{ value }, { error, touched }, { setValue, setTouched }] = useField<
-    undefined | DateString
-  >({ name, validate });
-  const errorText = touched && error;
+    const [{ value }, { error, touched }, { setValue, setTouched }] = useField<
+      undefined | DateString
+    >({ name, validate });
+    const errorText = touched && error;
 
-  return (
-    <DateField
-      {...props}
-      name={name}
-      value={value}
-      format={format}
-      error={!!errorText}
-      disabled={disabled || isSubmitting}
-      helperText={errorText || helperText}
-      onChange={(nextValue) => {
-        onChange?.(nextValue);
-        setTouched(true, false);
-        setValue(nextValue.stringValue);
-      }}
-    />
-  );
-}
+    return (
+      <DateField
+        {...props}
+        ref={ref}
+        name={name}
+        value={value}
+        format={format}
+        error={!!errorText}
+        disabled={disabled || isSubmitting}
+        helperText={errorText || helperText}
+        onChange={(nextValue) => {
+          onChange?.(nextValue);
+          setTouched(true, false);
+          setValue(nextValue.stringValue);
+        }}
+      />
+    );
+  },
+);
