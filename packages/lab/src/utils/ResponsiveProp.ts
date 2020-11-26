2c50105a7d9a9537706a732f4156190fa7831364
Umidbek Karimov <uma.karimov@gmail.com>
2020-11-26 16:41:52 +0500

    WIP

--- packages/lab/src/utils/ResponsiveProp.ts
+++ packages/lab/src/utils/ResponsiveProp.ts
@@ -1,20 +1,29 @@
 import { useDeepEqualMemo } from '@superdispatch/hooks';
 
 export type ResponsivePropPrimitive = boolean | number | string;
+export type ResponsivePropTupleInit<
+  T extends ResponsivePropPrimitive
+> = readonly [mobile: T, tablet?: T, desktop?: T];
 export type ResponsivePropTuple<T extends ResponsivePropPrimitive> = readonly [
   mobile: T,
-  tablet?: T,
-  desktop?: T,
+  tablet: T,
+  desktop: T,
 ];
 
 export type ResponsiveProp<T extends ResponsivePropPrimitive> =
   | T
-  | ResponsivePropTuple<T>;
+  | ResponsivePropTupleInit<T>;
 
 export function toResponsivePropTuple<T extends ResponsivePropPrimitive>(
   prop: ResponsiveProp<T>,
 ): ResponsivePropTuple<T> {
-  return Array.isArray(prop) ? prop : [prop];
+  if (!Array.isArray(prop)) {
+    return [prop, prop, prop];
+  }
+
+  const [mobile, tablet = mobile, desktop = tablet] = prop;
+
+  return [mobile, tablet, desktop];
 }
 
 export function useResponsivePropTuple<T extends ResponsivePropPrimitive>(
