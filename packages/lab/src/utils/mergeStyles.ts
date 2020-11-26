9172d67c7947eaa5cb910f1df91fcf084b10036a
Umidbek Karimov <uma.karimov@gmail.com>
2020-11-26 17:03:32 +0500

    WIP

--- packages/lab/src/utils/mergeStyles.ts
+++ packages/lab/src/utils/mergeStyles.ts
@@ -5,17 +5,21 @@ export function mergeStyles(
   ...sources: Array<null | false | undefined | CSSObject>
 ): CSSObject {
   for (const source of sources) {
-    if (!source) continue;
-    for (const key in source) {
-      if (!Object.prototype.hasOwnProperty.call(source, key)) continue;
+    if (source) {
+      for (const key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          const stylesValue = styles[key];
+          const sourceValue = source[key];
 
-      const stylesValue = styles[key];
-      const sourceValue = source[key];
-
-      if (typeof stylesValue == 'object' && typeof sourceValue == 'object') {
-        mergeStyles(stylesValue, sourceValue);
-      } else {
-        styles[key] = source[key];
+          if (
+            typeof stylesValue == 'object' &&
+            typeof sourceValue == 'object'
+          ) {
+            mergeStyles(stylesValue, sourceValue);
+          } else {
+            styles[key] = source[key];
+          }
+        }
       }
     }
   }
