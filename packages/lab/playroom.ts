e2be599cb8fd1c008a5d99ceb09bf5ff903f8b8a
Umidbek Karimov <uma.karimov@gmail.com>
2020-12-21 17:16:03 +0500

    WIP

--- packages/lab/playroom.ts
+++ packages/lab/playroom.ts
@@ -1,6 +1,6 @@
 export { Box } from './src/box/Box';
-export { Column } from './src/column/Column';
-export { Columns } from './src/column/Columns';
+export { Column } from './src/columns/Column';
+export { Columns } from './src/columns/Columns';
 export { DescriptionItem } from './src/description-item/DescriptionItem';
 export { Inline } from './src/inline/Inline';
 export { Stack } from './src/stack/Stack';
