bc7d2ac78148b00d397fb746165957bab54fa80b
Umidbek Karimov <uma.karimov@gmail.com>
2020-12-21 17:20:32 +0500

    WIP

---
+++ cypress/integration/lab/TextBox.spec.ts
@@ -0,0 +1,15 @@
+it('take snapshots', () => {
+  cy.visitStorybook();
+
+  cy.takeStorySnapshot('Lab/TextBox', 'Basic', ['mobile', 'desktop']);
+
+  cy.takeStorySnapshot('Lab/TextBox', 'Responsive Alignment', [
+    'mobile',
+    'desktop',
+  ]);
+
+  cy.takeStorySnapshot('Lab/TextBox', 'Colors', ['mobile', 'desktop']);
+  cy.takeStorySnapshot('Lab/TextBox', 'Variants', ['mobile', 'desktop']);
+  cy.takeStorySnapshot('Lab/TextBox', 'No Wrap', ['desktop']);
+  cy.takeStorySnapshot('Lab/TextBox', 'Wrap Overflow', ['desktop']);
+});
