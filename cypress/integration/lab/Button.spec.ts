76c57bcba7b547441150401c81ae966ef36f1c25
Umidbek Karimov <uma.karimov@gmail.com>
2021-01-21 18:25:10 +0500

    WIP

--- cypress/integration/lab/Button.spec.ts
+++ cypress/integration/lab/Button.spec.ts
@@ -6,20 +6,3 @@ it('takes snapshots', () => {
   cy.takeStorySnapshot('Lab/Button', 'Basic', ['mobile', 'desktop']);
   cy.takeStorySnapshot('Lab/Button', 'Sizes', ['mobile', 'desktop']);
 });
-
-it('check focus states', () => {
-  cy.selectStory('Lab/Button', 'Basic');
-
-  for (const name of [
-    'Primary',
-    'Default',
-    'Neutral',
-    'Text',
-    'Critical',
-    'Inverted',
-  ]) {
-    cy.findByRole('button', { name }).focus();
-    cy.wait(300);
-    cy.takeSnapshots(`Lab/Button: Basic - Focus ${name}`);
-  }
-});
