b18513c4aa62b3b8dedb593b80817971658d381d
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-03-04 12:36:38 +0500

    feat(lab): [Box] support `display` and negative `margin`. (#162)

--- packages/lab/src/box/Box.tsx
+++ packages/lab/src/box/Box.tsx
@@ -2,6 +2,7 @@ import {
   Color,
   ColorProp,
   isColorProp,
+  NegativeSpaceProp,
   parseResponsiveProp,
   parseSpaceProp,
   ResponsiveProp,
@@ -32,14 +33,20 @@ function parseSpace(space: unknown): string {
 // Margins
 //
 
-export type MarginProp = SpaceProp | 'auto';
-function parseMargin(margin: unknown): string {
-  switch (margin as MarginProp) {
-    case 'auto':
-      return 'auto';
-    default:
-      return parseSpace(margin);
+export type MarginProp = 'auto' | SpaceProp | NegativeSpaceProp;
+function parseMargin(input: unknown): string {
+  if (input === 'auto') {
+    return input;
   }
+
+  let prefix = '';
+
+  if (typeof input == 'string' && input.startsWith('-')) {
+    prefix = '-';
+    input = input.slice(1);
+  }
+
+  return prefix + parseSpace(input);
 }
 
 //
@@ -64,6 +71,8 @@ const normalizeBorderWidth = createRuleNormalizer<BorderWidthProp>({
 //
 
 interface BoxRules {
+  display?: ResponsiveProp<Property.Display>;
+
   color?: ResponsiveProp<ColorProp>;
   backgroundColor?: ResponsiveProp<ColorProp>;
 
@@ -113,6 +122,8 @@ interface BoxRules {
 }
 
 const normalizers: Record<keyof BoxRules, undefined | RuleNormalizer> = {
+  display: undefined,
+
   color: normalizeColor,
   backgroundColor: normalizeColor,
 
