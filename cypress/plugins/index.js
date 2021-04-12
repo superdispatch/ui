31dbfb10a00f3f23838569d62fac1e16df4dafda
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-04-12 12:46:46 +0500

    chore: Bump percy.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,12 +1,8 @@
 'use strict';
 
-const percyHealthCheck = require('@percy/cypress/task');
-
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  on('task', percyHealthCheck);
-
   return config;
 };
